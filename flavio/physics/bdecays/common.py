7a4a4ccd9448f1478f60a2d8680288b8d3d46a7e
David Straub <david.straub@tum.de>
2016-01-12 16:48:15 +0100

    [physics] Added file with common functions for B decays.

---
+++ flavio/physics/bdecays/common.py
@@ -0,0 +1,30 @@
+from math import sqrt
+
+"""Common functions needed for B decays."""
+
+def lambda_K(a,b,c):
+    r"""Källén function $\lambda$.
+
+    $\lambda(a,b,c) = a^2 + b^2 + c^2 - 2 (ab + bc + ac)$
+    """
+    return a**2 + b**2 + c**2 - 2*(a*b + b*c + a*c)
+
+def beta_l(ml, q2):
+    if q2 == 0:
+        return 0.
+    return sqrt(1. - (4*ml**2)/q2)
+
+wcsm = {
+'C7eff': -0.2909,
+'C7eff': -0.1596,
+'C9': 4.062,
+'C10': -4.189,
+}
+
+meson_quark = {
+('B+','K+'): 'bs',
+}
+
+meson_ff = {
+('B+','K+'): 'B->K',
+}
