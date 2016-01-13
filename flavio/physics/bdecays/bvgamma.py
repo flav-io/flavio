feb6b363ad5afcda1f4151532301c45845dbd16d
David Straub <david.straub@tum.de>
2016-01-13 15:46:44 +0100

    [physics] 1st version of functions for B->V gamma at LO.

---
+++ flavio/physics/bdecays/bvgamma.py
@@ -0,0 +1,64 @@
+from math import sqrt,pi
+from cmath import exp
+import numpy as np
+from flavio.physics.bdecays.common import meson_quark, meson_ff, wcsm
+from flavio.physics import ckm
+from flavio.physics.bdecays.formfactors import FormFactorParametrization as FF
+
+"""Functions for exclusive $B\to V\gamma$ decays."""
+
+def prefactor(par, B, V):
+    mB = par[('mass',B)]
+    mV = par[('mass',V)]
+    aem = par['alphaem']
+    mb = par[('mass','b','MSbar')]
+    GF = par['Gmu']
+    di_dj = meson_quark[(B,V)]
+    xi_t = ckm.xi('t',di_dj)(par)
+    return ( sqrt((GF**2 * aem * mB**3 * mb**2)/(32 * pi**4)
+                  * (1-mV**2/mB**2)**3) * xi_t )
+
+def amps(wc, par, B, V):
+    N = prefactor(par, B, V)
+    ff = FF.parametrizations['bsz3'].get_ff(meson_ff[(B,V)], 0, par)
+    c7 = wcsm['C7eff'] + wc['C7']
+    c7p = wc['C7p']
+    a = {}
+    a['L'] = N * c7  * ff['T1']
+    a['R'] = N * c7p * ff['T1']
+    return a
+
+def amps_bar(wc, par, B, V):
+    # FIXME need to implement CP conjugation
+    return amps(wc, par, B, V)
+
+def Gamma(a):
+    return ( abs(a['L'])**2 + abs(a['R'])**2 )
+
+def Gamma_CPaverage(a, a_bar):
+    return ( Gamma(a) + Gamma(a_bar) )/2.
+
+def BR(wc, par, B, V):
+    tauB = par[('lifetime',B)]
+    a = amps(wc, par, B, V)
+    return tauB * Gamma(a)
+
+def BR_CPaverage(wc, par, B, V):
+    tauB = par[('lifetime',B)]
+    a = amps(wc, par, B, V)
+    a_bar = amps_bar(wc, par, B, V)
+    return tauB * Gamma_CPaverage(a, a_bar)
+
+def ACP(wc, par, B, V):
+    a = amps(wc, par, B, V)
+    a_bar = amps_bar(wc, par, B, V)
+    return ( Gamma(a) - Gamma(a_bar) )/( Gamma(a) + Gamma(a_bar) )
+
+def S(wc, par, B, V):
+    a = amps(wc, par, B, V)
+    a_bar = amps_bar(wc, par, B, V)
+    beta = ckm.get_ckmangle_beta(par)
+    q_over_p  = exp(-2j*beta)
+    interf = a['L'].conj()*a_bar['L']+a['R'].conj()*a_bar['R']
+    Gav = Gamma_CPaverage(a, a_bar)
+    return q_over_p * interf / Gav
---
+++ flavio/physics/bdecays/test_bvgamma.py
@@ -0,0 +1,52 @@
+import unittest
+import numpy as np
+from .bvgamma import *
+from flavio.physics.bdecays.formfactors.b_v import bsz_parameters
+
+
+s = 1.519267515435317e+24
+
+par = {
+    ('mass','B+'): 5.27929,
+    ('mass','B0'): 5.27961,
+    ('mass','Bs'): 5.36679,
+    ('mass','K*0'): 0.89581,
+    ('mass','K*+'): 0.89166,
+    ('lifetime','B+'): 1638.e-15*s,
+    ('lifetime','B0'): 152.e-14*s,
+    ('mass','b','MSbar'): 4.18,
+    'Gmu': 1.1663787e-5,
+    'alphaem': 1/127.940,
+    'Vus': 0.22,
+    'Vub': 3.7e-3,
+    'Vcb': 4.1e-2,
+    'gamma': 1.22,
+}
+
+par.update(bsz_parameters.ffpar_lcsr)
+
+wc = {
+    'C7': 0,
+    'C7p': 0,
+    'C9': 0,
+    'C9p': 0,
+    'C10': 0,
+    'C10p': 0,
+    'CP': 0,
+    'CPp': 0,
+    'CS': 0,
+    'CSp': 0,
+}
+
+class TestBVgamma(unittest.TestCase):
+    def test_bksgamma(self):
+        # just some trivial tests to see if calling the functions raises an error
+        prefactor(par, 'B0', 'K*0')
+        a = amps(wc, par, 'B0', 'K*0')
+        S(wc, par, 'B0', 'K*0')
+        # ACP should vanish as CP conjugation is ignored here (a=abar)
+        self.assertEqual(ACP(wc, par, 'B0', 'K*0'),   0.)
+        # # rough numerical comparison of CP-averaged observables to 1503.05534v1
+        # # FIXME this should work much better with NLO corrections ...
+        self.assertAlmostEqual(BR(wc, par, 'B0', 'K*0')*1e4/4.22, 1, places=-1)
+        self.assertAlmostEqual(BR(wc, par, 'B+', 'K*+')*1e4/4.42, 1, places=-1)
