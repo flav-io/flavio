1397357d8248076ad2136c22eb4fedbc7fb11d6a
David Straub <david.straub@tum.de>
2016-01-13 17:06:40 +0100

    [physics] Error in BSZ form factors corrected

--- flavio/physics/bdecays/formfactors/b_v/btov.py
+++ flavio/physics/bdecays/formfactors/b_v/btov.py
@@ -1,19 +1,22 @@
-from flavio.physics.bdecays.formfactors.b_v import bsz
+from flavio.physics.bdecays.formfactors.b_v import bsz, lattice
 from flavio.physics.bdecays.formfactors.common import FormFactorParametrization
 
 FFs = ["A0","A1","A12","V","T1","T2","T23"]
 
-bsz_parnames = [ ff + '_' + a for ff in FFs for a in ["a0","a1","a2"]]
+bsz_parnames = [ a + '_' + ff for ff in FFs for a in ["a0","a1","a2"]]
 bsz_processes = ['B->K*','B->rho','B->omega','Bs->phi','Bs->K*']
+
+lattice_parnames = [ ff.lower() + '_' + a for ff in FFs for a in ["a0","a1","a2"]]
+lattice_processes = ['B->K*','Bs->phi','Bs->K*']
 
 bsz2 = FormFactorParametrization(name='bsz2',
                                  transition='b_v',
                                  processes=bsz_processes,
                                  parameters=bsz_parnames,
-                                 function=lambda process, q2, par: bsz.ff(process, q2, par, n=1))
+                                 function=lambda process, q2, par: bsz.ff(process, q2, par, n=2))
 
 bsz3 = FormFactorParametrization(name='bsz3',
                                  transition='b_v',
                                  processes=bsz_processes,
                                  parameters=bsz_parnames,
-                                 function=lambda process, q2, par: bsz.ff(process, q2, par, n=2))
+                                 function=lambda process, q2, par: bsz.ff(process, q2, par, n=3))
