951500b773ced8c49114974170732bb525c8d987
David Straub <david.straub@tum.de>
2016-01-19 16:43:13 +0100

    Added global config file

---
+++ flavio/config.py
@@ -0,0 +1,12 @@
+config={}
+
+config['bdecays']={}
+
+# renormalization scale for B->Pl+l-
+config['bdecays']['scale_bpll'] = 4.8
+
+# renormalization scale for B->l+l-
+config['bdecays']['scale_bll'] = 4.8
+
+# renormalization scale for B->Vl+l-
+config['bdecays']['scale_bvll'] = 4.8
