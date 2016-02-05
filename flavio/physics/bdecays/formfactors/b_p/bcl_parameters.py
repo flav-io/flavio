ebfb6d9115a2d1d6645896aa062ff85252d609de
David Straub <david.straub@tum.de>
2016-02-05 18:19:41 +0100

    [bdecays] Added data file for B->K BCL lattice form factors

---
+++ flavio/physics/bdecays/formfactors/b_p/bcl_parameters.py
@@ -0,0 +1,18 @@
+import yaml
+import pkgutil
+import numpy as np
+from flavio.classes import Parameter, MultivariateNormalDistribution
+
+def load_parameters(filename, constraints):
+    f = pkgutil.get_data('flavio.physics', filename)
+    ff_dict = yaml.load(f)
+    for parameter_name in ff_dict['parameters']:
+        try: # check if parameter object already exists
+            p = Parameter.get_instance(parameter_name)
+        except: # otherwise, create a new one
+            p = Parameter(parameter_name)
+        else: # if parameter exists, remove existing constraints
+            constraints.remove_constraints(parameter_name)
+    covariance = np.outer(ff_dict['uncertainties'], ff_dict['uncertainties'])*ff_dict['correlation']
+    constraints.add_constraint(ff_dict['parameters'],
+            MultivariateNormalDistribution(central_value=ff_dict['central_values'], covariance=covariance) )
--- flavio/physics/bdecays/formfactors/b_p/test_btop.py
+++ flavio/physics/bdecays/formfactors/b_p/test_btop.py
@@ -1,30 +1,13 @@
 import unittest
 from math import sqrt,radians,asin
-from flavio.physics.bdecays.formfactors.b_p import btop
+from flavio.physics.bdecays.formfactors.b_p import btop, bcl_parameters
 import numpy as np
 import copy
 from flavio.parameters import default_parameters
 from flavio.classes import Parameter, Implementation
 
 c = copy.copy(default_parameters)
-Parameter('B->K BCL a0_f+')
-Parameter('B->K BCL a0_f0')
-Parameter('B->K BCL a0_fT')
-Parameter('B->K BCL a1_f+')
-Parameter('B->K BCL a1_f0')
-Parameter('B->K BCL a1_fT')
-Parameter('B->K BCL a2_f+')
-Parameter('B->K BCL a2_f0')
-Parameter('B->K BCL a2_fT')
-c.set_constraint('B->K BCL a0_f+', 0.466)
-c.set_constraint('B->K BCL a1_f+', -0.885)
-c.set_constraint('B->K BCL a2_f+', -0.213)
-c.set_constraint('B->K BCL a0_f0', 0.292)
-c.set_constraint('B->K BCL a1_f0', 0.281)
-c.set_constraint('B->K BCL a2_f0', 0.150)
-c.set_constraint('B->K BCL a0_fT', 0.460)
-c.set_constraint('B->K BCL a1_fT', -1.089)
-c.set_constraint('B->K BCL a2_fT', -1.114)
+bcl_parameters.load_parameters('data/arxiv-1509-06235v1/b_k.yml', c)
 
 class TestBtoP(unittest.TestCase):
     def test_lattice(self):
