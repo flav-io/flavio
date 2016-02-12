5f24be0fb04a650585d6c834e803892ac2c2eb5a
David Straub <david.straub@tum.de>
2016-02-12 09:49:44 +0100

    [plots] New density contour plot function

--- flavio/plots/config.py
+++ flavio/plots/config.py
@@ -1,4 +1,7 @@
-from matplotlib import rc
+from matplotlib import rc, rcParams
 
 rc('text', usetex=True)
 rc('font',**{'family':'serif','serif':['Computer Modern Roman']})
+rcParams['xtick.labelsize'] = 10
+rcParams['ytick.labelsize'] = 10
+rcParams['axes.labelsize'] = 14
--- flavio/plots/plots.py
+++ flavio/plots/plots.py
@@ -1,7 +1,9 @@
 from collections import OrderedDict
-import flavio
 import matplotlib.pyplot as plt
-
+import numpy as np
+import flavio
+from scipy.optimize import brentq
+from scipy.stats import gaussian_kde
 
 def error_budget_pie(err_dict, other_cutoff=0.03):
     """Pie chart of an observable's error budget."""
@@ -24,3 +26,28 @@ def error_budget_pie(err_dict, other_cutoff=0.03):
         return r'{p:.1f}\%'.format(p=pct*err_tot)
     plt.axis('equal')
     return plt.pie(fracs, labels=labels, autopct=my_autopct, wedgeprops = {'linewidth':0.5}, colors=flavio.plots.colors.pastel)
+
+
+def find_confidence_interval(x, pdf, confidence_level):
+    return pdf[pdf > x].sum() - confidence_level
+
+def density_contour_kernel(x, y, xmin, xmax, ymin, ymax, covariance_factor=None):
+    xx, yy = np.mgrid[xmin:xmax:100j, ymin:ymax:100j]
+    positions = np.vstack([xx.ravel(), yy.ravel()])
+    values = np.vstack([x, y])
+    kernel = gaussian_kde(values)
+
+    if covariance_factor is not None:
+        kernel.covariance_factor = lambda: covariance_factor
+        kernel._compute_covariance()
+
+    f = np.reshape(kernel(positions).T, xx.shape)*(ymax-ymin)*(xmax-xmin)/1e4
+
+    one_sigma = brentq(find_confidence_interval, 0., 1., args=(f.T, 0.68))
+    two_sigma = brentq(find_confidence_interval, 0., 1., args=(f.T, 0.95))
+    levels = [two_sigma, one_sigma, np.max(f)]
+    print(levels)
+
+    ax = plt.gca()
+    ax.contourf(xx, yy, f, levels=levels, colors=flavio.plots.colors.reds[3:])
+    ax.contour(xx, yy, f, levels=levels, colors=flavio.plots.colors.reds[::-1], linewidths=0.7)
