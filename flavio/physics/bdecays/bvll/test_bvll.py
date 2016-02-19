24222c677fa1c440d07d6479435cfdb50b36e194
David Straub <david.straub@tum.de>
2016-02-19 17:15:46 +0100

    [bvll] Added numerical comparison to David's old Mathematica code.

--- flavio/physics/bdecays/bvll/test_bvll.py
+++ flavio/physics/bdecays/bvll/test_bvll.py
@@ -1,44 +1,32 @@
 import unittest
 import numpy as np
-from .amplitudes import *
-from .observables import *
-from .qcdf import *
+import flavio
 from flavio.physics.bdecays.formfactors.b_v import bsz_parameters
-from flavio.physics.eft import WilsonCoefficients
-from flavio.physics.bdecays.wilsoncoefficients import wctot_dict
-from flavio.config import config
-from flavio.physics.running import running
-from flavio.parameters import default_parameters
-import copy
-
-s = 1.519267515435317e+24
-
-
-c = copy.copy(default_parameters)
-bsz_parameters.bsz_load_v1_lcsr(c)
-par = c.get_central_all()
-
-wc_obj = WilsonCoefficients()
-wc = wctot_dict(wc_obj, 'bsmumu', 4.2, par)
 
 class TestBVll(unittest.TestCase):
-    def test_bksll(self):
-        # just some trivial tests to see if calling the functions raises an error
-        q2 = 3.5
-        h = helicity_amps(q2, wc_obj, par, 'B0', 'K*0', 'mu')
-        scale = config['renormalization scale']['bvll']
-        ml = par['m_mu']
-        mB = par['m_B0']
-        mV = par['m_K*0']
-        mb = running.get_mb(par, scale)
-        J = angular.angularcoeffs_general_v(h, q2, mB, mV, mb, 0, ml, ml)
-        # A7 should vanish as CP conjugation is ignored here (J=Jbar)
-        self.assertEqual(A_experiment(J, J, 7),   0.)
-        # rough numerical comparison of CP-averaged observables to 1503.05534v1
-        # FIXME this should work much better with NLO corrections ...
-        self.assertAlmostEqual(S_experiment(J, J, 4),  -0.151, places=1)
-        self.assertAlmostEqual(S_experiment(J, J, 5),  -0.212, places=1)
-        self.assertAlmostEqual(AFB_experiment(J, J),    0.002, places=1)
-        self.assertAlmostEqual(FL(J, J),                0.820, places=1)
-        BR = bvll_dbrdq2(q2, wc_obj, par, 'B0', 'K*0', 'mu') * 1e7
-        self.assertAlmostEqual(BR, 0.467, places=1)
+    def test_compare_to_Davids_old_code(self):
+        # this actually works to 3% and better! Still, setting it to 15%
+        # here to not give any error in case of future changes to the central value of the code
+        delta = 0.15
+        self.assertAlmostEqual(flavio.sm_prediction("AFB(B0->K*mumu)", q2=1)/-0.15349451759556973, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("FL(B0->K*mumu)", q2=1)/0.6406629242990924, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("S5(B0->K*mumu)", q2=1)/0.22071783227496475, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("P4p(B0->K*mumu)", q2=1)/0.15140284164390483, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("P5p(B0->K*mumu)", q2=1)/0.5064923738851054, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("AFB(B0->K*mumu)", q2=6)/0.17920578512694066, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("FL(B0->K*mumu)", q2=6)/0.6746725598733865, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("S4(B0->K*mumu)", q2=6)/-0.24476949751293892, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("S5(B0->K*mumu)", q2=6)/-0.3735858481345886, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("P4p(B0->K*mumu)", q2=6)/-0.529132300413707, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("P5p(B0->K*mumu)", q2=6)/-0.807602014278806, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("AFB(B0->K*mumu)", q2=15)/0.406194755197377, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("FL(B0->K*mumu)", q2=15)/0.36807161547568185, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("S3(B0->K*mumu)", q2=15)/-0.14872271241628923, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("S4(B0->K*mumu)", q2=15)/-0.2919191029733921, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("S5(B0->K*mumu)", q2=15)/-0.34293705381921147, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("P4p(B0->K*mumu)", q2=15)/-0.6065016593226283, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("P5p(B0->K*mumu)", q2=15)/-0.7124983944730857, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("dBR/dq2(B0->K*mumu)", q2=1)/5.7131974656129313e-8, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("dBR/dq2(B0->K*mumu)", q2=6)/5.524478678779278e-8, 1, delta=delta)
+        self.assertAlmostEqual(flavio.sm_prediction("dBR/dq2(B0->K*mumu)", q2=15)/7.412258847550239e-8, 1, delta=delta)
+        # self.assertAlmostEqual(flavio.sm_prediction("dBR/dq2(B+->K+mumu)", q2=15)/2.1824401629030333e-8, 1, delta=delta)
