9297d02428ceec24d5f9ef2512803418d894fca2
David Straub <david.straub@tum.de>
2016-02-23 22:23:24 +0100

    [plots] Load amsmath for LaTeX plot labels

--- flavio/plots/config.py
+++ flavio/plots/config.py
@@ -5,3 +5,4 @@ rc('font',**{'family':'serif','serif':['Computer Modern Roman']})
 rcParams['xtick.labelsize'] = 10
 rcParams['ytick.labelsize'] = 10
 rcParams['axes.labelsize'] = 14
+rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
