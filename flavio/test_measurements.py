b6af5f7e8286d6b6f6ad750e7cf33ad970ad1d06
David Straub <david.straub@tum.de>
2016-02-24 23:18:13 +0100

    [measurements] Added LHCb 3/fb B->K(*)mumu BRs

--- flavio/test_measurements.py
+++ flavio/test_measurements.py
@@ -6,5 +6,5 @@ from .classes import *
 
 class TestMeasurements(unittest.TestCase):
     def test_measurements(self):
-        m = Measurement.get_instance('CMS+LHCb Bs->mumu 2014')
+        m = Measurement.get_instance('CMS+LHCb Bq->mumu 2014')
         self.assertEqual(m.experiment, 'CMS+LHCb')
