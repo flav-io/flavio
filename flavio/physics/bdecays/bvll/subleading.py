1355573e50fee0765cd142e1e8e51abc14aae463
David Straub <david.straub@tum.de>
2016-06-24 10:39:27 +0200

    [bvll] New deltaC7,7' and deltaC9,7,7' parametrizations of subleading effects in B->Vll

--- flavio/physics/bdecays/bvll/subleading.py
+++ flavio/physics/bdecays/bvll/subleading.py
@@ -41,6 +41,21 @@ def helicity_amps_deltaC7(q2, deltaC7_dict, par, B, V):
         ha[amp] = _helicity_amps_deltaC(q2, deltaC7_dict[amp], '7', par, B, V)[amp]
     return ha
 
+def helicity_amps_deltaC7p(q2, deltaC7p_dict, par, B, V):
+    r"""A function returning a contribution to the helicity amplitudes in
+    $B\to V\ell^+\ell^-$ coming from an effective helicity-dependent shift of
+    the Wilson coefficient $C_7'(\mu_b)$. This can be used to parametrize
+    residual uncertainties due to subleading non-factorizable hadronic effects.
+
+    The input dictionary `deltaC7p_dict` should be of the form
+
+    `{ ('0','V'): deltaC7p_0, ('pl','V'): deltaC7p_pl, ('mi','V'): deltaC7p_mi }`
+    """
+    ha = {}
+    for amp in [('0','V'), ('pl','V'), ('mi','V')]:
+        ha[amp] = _helicity_amps_deltaC(q2, deltaC7p_dict[amp], '7p', par, B, V)[amp]
+    return ha
+
 def helicity_amps_deltaC9(q2, deltaC9_dict, par, B, V):
     r"""A function returning a contribution to the helicity amplitudes in
     $B\to V\ell^+\ell^-$ coming from an effective helicity-dependent shift of
@@ -68,6 +83,34 @@ def helicity_amps_deltaC7_polynomial(q2, par, B, V):
              +1j*( par[B+'->'+V+' deltaC7 a_- Im']  + par[B+'->'+V+' deltaC7 b_- Im'] *q2 ))
     deltaC7_dict = { ('0','V'): deltaC7_0, ('pl','V'): deltaC7_pl, ('mi','V'): deltaC7_mi }
     return helicity_amps_deltaC7(q2, deltaC7_dict, par, B, V)
+
+def helicity_amps_deltaC7C7p_polynomial(q2, par, B, V):
+    deltaC7_0   =( par[B+'->'+V+' deltaC7 a_0 Re']  + par[B+'->'+V+' deltaC7 b_0 Re'] *q2
+             +1j*( par[B+'->'+V+' deltaC7 a_0 Im']  + par[B+'->'+V+' deltaC7 b_0 Im'] *q2 ))
+    deltaC7p_pl  =( par[B+'->'+V+' deltaC7p a_+ Re']  + par[B+'->'+V+' deltaC7p b_+ Re'] *q2
+             +1j*( par[B+'->'+V+' deltaC7p a_+ Im']  + par[B+'->'+V+' deltaC7p b_+ Im'] *q2 ))
+    deltaC7_mi  =( par[B+'->'+V+' deltaC7 a_- Re']  + par[B+'->'+V+' deltaC7 b_- Re'] *q2
+             +1j*( par[B+'->'+V+' deltaC7 a_- Im']  + par[B+'->'+V+' deltaC7 b_- Im'] *q2 ))
+    deltaC7_dict = { ('0','V'): deltaC7_0, ('pl','V'): 0, ('mi','V'): deltaC7_mi }
+    deltaC7p_dict = { ('0','V'): 0, ('pl','V'): deltaC7p_pl, ('mi','V'): 0 }
+    ha_deltaC7 = helicity_amps_deltaC7(q2, deltaC7_dict, par, B, V)
+    ha_deltaC7p = helicity_amps_deltaC7p(q2, deltaC7p_dict, par, B, V)
+    return add_dict([ha_deltaC7, ha_deltaC7p])
+
+def helicity_amps_deltaC9C7C7p_polynomial(q2, par, B, V):
+    deltaC9_0   =( par[B+'->'+V+' deltaC9 a_0 Re']  + par[B+'->'+V+' deltaC9 b_0 Re'] *q2
+             +1j*( par[B+'->'+V+' deltaC9 a_0 Im']  + par[B+'->'+V+' deltaC9 b_0 Im'] *q2 ))
+    deltaC7p_pl  =( par[B+'->'+V+' deltaC7p a_+ Re']  + par[B+'->'+V+' deltaC7p b_+ Re'] *q2
+             +1j*( par[B+'->'+V+' deltaC7p a_+ Im']  + par[B+'->'+V+' deltaC7p b_+ Im'] *q2 ))
+    deltaC7_mi  =( par[B+'->'+V+' deltaC7 a_- Re']  + par[B+'->'+V+' deltaC7 b_- Re'] *q2
+             +1j*( par[B+'->'+V+' deltaC7 a_- Im']  + par[B+'->'+V+' deltaC7 b_- Im'] *q2 ))
+    deltaC9_dict = { ('0','V'): deltaC9_0, ('pl','V'): 0, ('mi','V'): 0 }
+    deltaC7_dict = { ('0','V'): 0, ('pl','V'): 0, ('mi','V'): deltaC7_mi }
+    deltaC7p_dict = { ('0','V'): 0, ('pl','V'): deltaC7p_pl, ('mi','V'): 0 }
+    ha_deltaC9 = helicity_amps_deltaC7(q2, deltaC9_dict, par, B, V)
+    ha_deltaC7 = helicity_amps_deltaC7(q2, deltaC7_dict, par, B, V)
+    ha_deltaC7p = helicity_amps_deltaC7p(q2, deltaC7p_dict, par, B, V)
+    return add_dict([ha_deltaC7, ha_deltaC7p])
 
 # note that, when parametrizing the correction as a shift to C9 rather than C7,
 # the contribution to the transverse (+ and -) amplitudes has to start with
@@ -98,6 +141,22 @@ def fct_deltaC7_polynomial(B, V):
         if cp_conjugate:
             par = conjugate_par(par)
         return helicity_amps_deltaC7_polynomial(q2, par, B, V)
+    return fct
+
+def fct_deltaC7C7p_polynomial(B, V):
+    def fct(wc_obj, par_dict, q2, cp_conjugate):
+        par = par_dict.copy()
+        if cp_conjugate:
+            par = conjugate_par(par)
+        return helicity_amps_deltaC7C7p_polynomial(q2, par, B, V)
+    return fct
+
+def fct_deltaC9C7C7p_polynomial(B, V):
+    def fct(wc_obj, par_dict, q2, cp_conjugate):
+        par = par_dict.copy()
+        if cp_conjugate:
+            par = conjugate_par(par)
+        return helicity_amps_deltaC9C7C7p_polynomial(q2, par, B, V)
     return fct
 
 def fct_deltaC9_polynomial(B, V):
@@ -131,6 +190,25 @@ for had in [('B0','K*0'), ('B+','K*+'), ('Bs','phi'), ]:
     i = Implementation(name=iname, quantity=quantity,
                    function=fct_deltaC7_polynomial(B=had[0], V=had[1]))
     i.set_description(r"Effective shift in the Wilson coefficient $C_7(\mu_b)$"
+                      r" as a first-order polynomial in $q^2$.")
+
+    # Implementation: C7-C7'-polynomial
+    iname = process + ' deltaC7, 7p polynomial'
+    i = Implementation(name=iname, quantity=quantity,
+                   function=fct_deltaC7C7p_polynomial(B=had[0], V=had[1]))
+    i.set_description(r"Effective shift in the Wilson coefficient $C_7(\mu_b)$"
+                      r" (in the $0$ and $-$ helicity amplitudes) and"
+                      r" $C_7'(\mu_b)$ (in the $+$ helicity amplitude)"
+                      r" as a first-order polynomial in $q^2$.")
+
+    # Implementation: C9-C7-C7'-polynomial
+    iname = process + ' deltaC9, 7, 7p polynomial'
+    i = Implementation(name=iname, quantity=quantity,
+                   function=fct_deltaC9C7C7p_polynomial(B=had[0], V=had[1]))
+    i.set_description(r"Effective shift in the Wilson coefficient $C_9(\mu_b)$"
+                      r" (in the $0$ helicity amplitude), $C_7(\mu_b)$"
+                      r" (in the $-$ helicity amplitude) and"
+                      r" $C_7'(\mu_b)$ (in the $+$ helicity amplitude)"
                       r" as a first-order polynomial in $q^2$.")
 
     # Implementation: C9-polynomial
