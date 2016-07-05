a17bd430bdba28ba1f25878f9c4d2c9c72d7165d
David Straub <david.straub@tum.de>
2016-07-05 13:56:50 +0200

    [blnu] Small changes to B->lnu unit tests

--- flavio/physics/bdecays/test_blnu.py
+++ flavio/physics/bdecays/test_blnu.py
@@ -13,13 +13,13 @@ class TestBlnu(unittest.TestCase):
         self.assertAlmostEqual(
             flavio.Observable.get_instance("BR(B+->taunu)").prediction_central(constraints, wc_obj),
             1.1e-4 * (abs(Vub)/3.95e-3)**2 * (par['f_B+']/0.2)**2,
-            places=2)
+            delta=2e-6)
         # check that B->enu BR is smaller than B->munu
         # (ratio given by mass ratio squared)
         self.assertAlmostEqual(
             (
             flavio.Observable.get_instance("BR(B+->enu)").prediction_central(constraints, wc_obj)/
             flavio.Observable.get_instance("BR(B+->munu)").prediction_central(constraints, wc_obj)
-            ),
-            par['m_e']**2/par['m_mu']**2,
-            places=3) # only 3 places as there are corrections of order mmu**2/mB**2
+            )/(par['m_e']**2/par['m_mu']**2),
+            1,
+            delta=0.001) # there are corrections of order mmu**2/mB**2
