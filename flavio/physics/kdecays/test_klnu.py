a6787aaf4f4fe573cee7bae72f5b02e8c6d97005
David Straub <david.straub@tum.de>
2016-07-11 17:02:55 +0200

    [klnu] Adapt test to account for change in Vus

--- flavio/physics/kdecays/test_klnu.py
+++ flavio/physics/kdecays/test_klnu.py
@@ -15,7 +15,7 @@ class TestKlnu(unittest.TestCase):
             1.582e-5, delta=3*0.007e-5)
         self.assertAlmostEqual(
             flavio.Observable.get_instance("BR(K+->munu)").prediction_central(constraints, wc_obj),
-            63.56e-2, delta=3*0.11e-2)
+            63.56e-2, delta=8*0.11e-2)
 
     def test_rklnu(self):
         # compare to 0707.4464
@@ -31,7 +31,7 @@ class TestKlnu(unittest.TestCase):
             1.23e-4+7.39e-7, delta=3e-6)
         self.assertAlmostEqual(
             flavio.Observable.get_instance("BR(pi+->munu)").prediction_central(constraints, wc_obj),
-            99.9877e-2, delta=0.02)
+            99.9877e-2, delta=2*0.02)
 
     def test_rpilnu(self):
         # compare to 0707.4464
