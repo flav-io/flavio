dc3006c25a4e20be2c323fe2c80072f650476423
David Straub <david.straub@tum.de>
2016-09-27 09:03:20 +0200

    Revert "[classes] Remove unneeded import"
    This reverts commit 55be6ef5b1c4154ffd71af941aa4e5513aecd220.

--- flavio/classes.py
+++ flavio/classes.py
@@ -6,7 +6,7 @@ from .config import config
 from collections import OrderedDict
 import copy
 import math
-from flavio._parse_errors import constraints_from_string, combine_distributions, string_from_constraints
+from flavio._parse_errors import constraints_from_string, combine_distributions, errors_from_string, string_from_constraints
 import scipy.stats
 
 def _is_number(s):
