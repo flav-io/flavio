1ba312c3898ac5bd55599960c67a3462fa109523
David Straub <david.straub@tum.de>
2017-01-23 14:59:42 +0100

    [bxll] Added forward-backward asymmetry in B->Xll

--- flavio/physics/bdecays/bxll.py
+++ flavio/physics/bdecays/bxll.py
@@ -15,14 +15,15 @@ def bxll_parameters(par, lep):
     ml = par['m_'+lep]
     scale = flavio.config['renormalization scale']['bxll']
     mb = flavio.physics.running.running.get_mb_pole(par)
+    mc = flavio.physics.running.running.get_mc_pole(par)
     alpha = flavio.physics.running.running.get_alpha(par, scale)
     alpha_s = alpha['alpha_s']
     alpha_e = alpha['alpha_e']
-    return ml, mb, alpha_s, alpha_e
+    return ml, mb, alpha_s, alpha_e, mc
 
 def bxll_br_prefactor(par, q, lep):
     # prefactor for the branching ratio normalized to B->X_c enu
-    ml, mb, alpha_s, alpha_e = bxll_parameters(par, lep)
+    ml, mb, alpha_s, alpha_e, mc = bxll_parameters(par, lep)
     bq = 'b' + q
     xi_t = flavio.physics.ckm.xi('t',bq)(par)
     Vcb = flavio.physics.ckm.get_ckm(par)[1,2]
@@ -65,12 +66,12 @@ def inclusive_wc(q2, wc_obj, par, q, lep, mb):
     wc_eff['tp'] = 0
     return {'wc_eff': wc_eff, 'wc': wc}
 
-def _bxll_dbrdq2(q2, wc_obj, par, q, lep, include_qed=True, include_pc=True):
-    # see below: bxll_dbrdq2()
-    ml, mb, alpha_s, alpha_e = bxll_parameters(par, lep)
+def _bxll_dbrdq2_unnorm(q2, wc_obj, par, q, lep, include_qed=True, include_pc=True):
+    # unnormalized PhiLL
+    # see below: _bxll_dbrdq2
+    ml, mb, alpha_s, alpha_e, mc = bxll_parameters(par, lep)
     if q2 < 4*ml**2 or q2 > mb**2:
         return 0
-    N = bxll_br_prefactor(par, q, lep)
     wc_dict = inclusive_wc(q2, wc_obj, par, q, lep, mb)
     wc_eff = wc_dict['wc_eff']
     wc = wc_dict['wc']
@@ -82,11 +83,20 @@ def _bxll_dbrdq2(q2, wc_obj, par, q, lep, include_qed=True, include_pc=True):
     Phi_ll += f_ll(sh, mlh, alpha_s, wc_eff['7p'], wc_eff['vp'], wc_eff['ap'], wc_eff['sp'], wc_eff['pp'])
     if include_qed:
         # log-enhanced QED corrections: BR ~ H_L + H_T
-        Phi_ll += Phill_logqed('BR', sh, mb, ml, alpha_s, alpha_e, wc, q, lep)
+        Phi_ll += Phill_logqed('BR', sh, mb, ml, alpha_s, alpha_e, wc, q, lep, mc)
     if include_pc:
         # 1/mb^2 power correction: BR ~ H_L + H_T
         Phi_ll += Phill_pc_mb2('L', sh, par['lambda_1'], par['lambda_2'], mb, wc, q, lep)
         Phi_ll += Phill_pc_mb2('T', sh, par['lambda_1'], par['lambda_2'], mb, wc, q, lep)
+    return Phi_ll
+
+def _bxll_dbrdq2(q2, wc_obj, par, q, lep, include_qed=True, include_pc=True):
+    # see below: bxll_dbrdq2()
+    ml, mb, alpha_s, alpha_e, mc = bxll_parameters(par, lep)
+    if q2 < 4*ml**2 or q2 > mb**2:
+        return 0
+    N = bxll_br_prefactor(par, q, lep)
+    Phi_ll  = _bxll_dbrdq2_unnorm(q2, wc_obj, par, q, lep, include_qed=include_qed, include_pc=include_pc)
     # denominator of the normalisation to the B->X_u lnu rate
     Phi_u = f_u(alpha_e, alpha_s, par['alpha_s'], mb, par['lambda_1'], par['lambda_2'])
     # fudge factor to account for remaining uncertainty
@@ -110,6 +120,72 @@ def bxll_dbrdq2(q2, wc_obj, par, q, lep):
     else:
         return _bxll_dbrdq2(q2, wc_obj, par, q, lep)
 
+def _bxll_afb_num(q2, wc_obj, par, q, lep, include_qed=True, include_pc=True):
+    # see below: bxll_afb()
+    ml, mb, alpha_s, alpha_e, mc = bxll_parameters(par, lep)
+    if q2 < 4*ml**2 or q2 > mb**2:
+        return 0
+    wc_dict = inclusive_wc(q2, wc_obj, par, q, lep, mb)
+    wc_eff = wc_dict['wc_eff']
+    wc = wc_dict['wc']
+    sh = q2/mb**2
+    mlh = ml/mb
+    # LO contributions + bremsstrahlung
+    Phi_ll  = f_ll_ha(sh, mlh, alpha_s, wc_eff)
+    if include_qed and q2 < 14: # log-enhanced QED corrections to AFB only known for low q2!
+        # log-enhanced QED corrections: BR ~ H_L + H_T
+        Phi_ll += Phill_logqed('A', sh, mb, ml, alpha_s, alpha_e, wc, q, lep, mc)
+    if include_pc:
+        # 1/mb^2 power correction: BR ~ H_L + H_T
+        Phi_ll += Phill_pc_mb2('A', sh, par['lambda_1'], par['lambda_2'], mb, wc, q, lep)
+    # fudge factor to account for remaining uncertainty
+    if q2 < 12:
+        if lep == 'tau':
+            unc = 1 # for the tau, there is now low q^2 anyway
+        else:
+            unc = 1 + par['delta_BX'+q+lep+lep+' low']
+    else:
+        unc = 1 + par['delta_BX'+q+lep+lep+' high']
+    return 3/4. * Phi_ll * unc
+
+def bxll_afb_num(q2, wc_obj, par, q, lep, **kwargs):
+    r"""Inclusive $B\to  X_q\ell^+\ell^-$ normalized differential
+    forward-backward asymmetry."""
+    if q2 > 8 and q2 < 14:
+        warnings.warn("The predictions in the region of narrow charmonium resonances are not meaningful")
+    if lep == 'l':
+        # average of e and mu!
+        return (_bxll_afb_num(q2, wc_obj, par, q, 'e', **kwargs) + _bxll_afb_num(q2, wc_obj, par, q, 'mu', **kwargs))/2.
+    else:
+        return _bxll_afb_num(q2, wc_obj, par, q, lep, **kwargs)
+
+def bxll_afb_den(q2, wc_obj, par, q, lep, **kwargs):
+    # auxiliary function needed for q2-integrated AFB defined below
+    if q2 > 8 and q2 < 14:
+        warnings.warn("The predictions in the region of narrow charmonium resonances are not meaningful")
+    if q2 > 14:
+        include_qed = False  # log-enhanced QED corrections to AFB only known for low q2!
+        kwargs.pop('include_qed', None)
+    else:
+        if 'include_qed' in kwargs:
+            include_qed = kwargs.pop('include_qed')
+        else:
+            include_qed = True
+    if lep == 'l':
+        # average of e and mu!
+         return (_bxll_dbrdq2_unnorm(q2, wc_obj, par, q, 'e', include_qed=include_qed, **kwargs)
+               + _bxll_dbrdq2_unnorm(q2, wc_obj, par, q, 'mu', include_qed=include_qed, **kwargs))/2.
+    else:
+        return _bxll_dbrdq2_unnorm(q2, wc_obj, par, q, lep, include_qed=include_qed, **kwargs)
+
+def bxll_afb(q2, wc_obj, par, q, lep, **kwargs):
+    num = bxll_afb_num(q2, wc_obj, par, q, lep, **kwargs)
+    denom = bxll_afb_den(q2, wc_obj, par, q, lep, **kwargs)
+    if denom == 0:
+        return 0.
+    else:
+        return num/denom
+
 def f_ll(sh, mlh, alpha_s, C7t, C9t, C10t, CS, CP):
     # The function of Wilson coefficients and kinematical quantities entering
     # the inclusive $B\to Xll$ rate.
@@ -125,6 +201,25 @@ def f_ll(sh, mlh, alpha_s, C7t, C9t, C10t, CS, CP):
         + 6*mlh**2 * (abs(C9t)**2 - abs(C10t)**2)
         + 6 * mlh * (CP * C10t.conjugate()).real )
 
+def f_ll_ha(sh, mlh, alpha_s, wc_eff):
+    # The function of Wilson coefficients and kinematical quantities entering
+    # the inclusive $B\to Xll$ forward-backward asymmetry
+    # NB: lepton mass is neglected here
+    C7 = wc_eff['7']
+    C9 = wc_eff['v']
+    C10 = wc_eff['a']
+    C7p = wc_eff['7p']
+    C9p = wc_eff['vp']
+    C10p = wc_eff['ap']
+    return -4*(1-sh)**2 * (
+        # here we add non-univ. bremsstrahlung corrections
+        + sh*(C9*C10.conjugate()).real *  (1 + alpha_s/pi * tau910(sh))
+        +  2*(C7*C10.conjugate()).real *  (1 + alpha_s/pi * tau710(sh))
+        # primed operators - assume same bremsstrahlung as for unprimed
+        - sh*(C9p*C10p.conjugate()).real *(1 + alpha_s/pi * tau910(sh))
+        -  2*(C7p*C10p.conjugate()).real *(1 + alpha_s/pi * tau710(sh))
+        )
+
 # Functions needed for bremsstrahlung corrections to rate
 def sigma9(s):
     return sigma(s) + 3/2
@@ -138,6 +233,14 @@ def tau99(s):
     return -4/(9 * (1+2 * s)) * (2 * (1-s)**2 * log(1-s) +(3 * s * (1+s) * (1-2 * s))/((1-s)**2) * log(s)+(3 * (1-3 * s**2))/(1-s) )
 def tau79(s):
     return - (4 * (1-s)**2)/(9 * s) * log(1-s)-(4 * s * (3-2 * s))/(9 * (1-s)**2) * log(s) -(2 * (5-3 * s))/(9 * (1-s))
+def tau710(s):
+	return -5/2 + 1/(3 * (1-3*s)) - 1/3 * (s * (6-7 * s) * log(s))/((1-s)**2)- 1/9 * ((3-7 * s + 4 * s**2) * log(1-s))/(s) + (f7(s))/3
+def tau910(s) :
+	return -5/2 + 1/(3 * (1-s)) - 1/3 * (s * (6-7*s) * log(s))/(((1-s)**2)) - 2/9 * ((3-5 * s + 2 * s**2) * log(1-s))/(s) + (f9(s))/3
+def f7(s):
+	return 1/(6 * (s-1)**2 ) * ( 24 * (1+13*s -4*s**2) * li2( sqrt(s)) + 12 * (1-17*s+6*s**2) * li2(s) +6*s * (6-7*s) * log(s) +24 * (1-s)**2*log(s) * log(1-s) + 12 * (-13+16*s-3*s**2) * (log(1- sqrt(s))-log(1-s)) +39 -2*pi**2 +252*s -26*pi**2*s +21*s**2+8*pi**2*s**2 -180 * sqrt(s) -132*s * sqrt(s) )
+def f9(s):
+	return -1/(6 * (s-1)**2 ) * ( 48 * s * (-5+ 2*s) * li2( sqrt(s)) + 24 * (-1+7*s-3*s**2) * li2(s) + 6*s * (-6+7*s) * log(s) -24 * (1-s)**2 * log(s) * log(1-s) +24 * (5-7*s+2*s**2) * (log(1- sqrt(s))-log(1-s)) -21-156*s+20*pi**2*s +9*s**2-8*pi**2*s**2+120 * sqrt(s)+48*s * sqrt(s) )
 
 def Phill_pc_mb2(I, sh, l1, l2, mb, wc, q, lep):
     # 1/mb^2 power correction to Phi_ll
@@ -171,7 +274,7 @@ def Phill_pc_mb2(I, sh, l1, l2, mb, wc, q, lep):
     return ( l1/mb**2 * np.dot(wc1_10, np.dot(chi1, wc1_10.conj()))
            + l2/mb**2 * np.dot(wc1_10, np.dot(chi2, wc1_10.conj())) ).real
 
-def Phill_logqed(I, sh, mb, ml, alpha_s, alpha_e, wc, q, lep):
+def Phill_logqed(I, sh, mb, ml, alpha_s, alpha_e, wc, q, lep, mc):
     # log-enhanced QED corrections to Phi_ll
     bq = 'b'+q
     bqll = bq + lep + lep
@@ -184,23 +287,23 @@ def Phill_logqed(I, sh, mb, ml, alpha_s, alpha_e, wc, q, lep):
     # Note the different powers of ast (\tilde\alpha_s) and k (\kappa) due to
     # the different normalisation of 1) C9 and C10 and 2) the overall
     # normalisation of phi_ll
-    e = np.zeros((11, 11)) # from 0 to 10
+    e = np.zeros((11, 11), dtype=complex) # from 0 to 10
     if I == 'T' or I == 'L' or I == 'BR':
-        e[7,7] = 8 * ast    * k    * sigmaij_I(I, 7, 7, sh)*wem(I, 7, 7, sh, mb, ml, scale)
-        e[7,9] = 8 * ast    * k    * sigmaij_I(I, 7, 9, sh)*wem(I, 7, 9, sh, mb, ml, scale)
-        e[9,9] = 8 * ast    * k    * sigmaij_I(I, 9, 9, sh)*wem(I, 9, 9, sh, mb, ml, scale)
-        e[2,9] = 8 * ast    * k    * sigmaij_I(I, 9, 9, sh)*wem(I, 2, 9, sh, mb, ml, scale)
-        e[2,2] = 8 * ast    * k    * sigmaij_I(I, 9, 9, sh)*wem(I, 2, 2, sh, mb, ml, scale)
+        e[7,7] = 8 * ast    * k    * sigmaij_I(I, 7, 7, sh)*wem(I, 7, 7, sh, mb, ml, scale, mc)
+        e[7,9] = 8 * ast    * k    * sigmaij_I(I, 7, 9, sh)*wem(I, 7, 9, sh, mb, ml, scale, mc)
+        e[9,9] = 8 * ast    * k    * sigmaij_I(I, 9, 9, sh)*wem(I, 9, 9, sh, mb, ml, scale, mc)
+        e[2,9] = 8 * ast    * k    * sigmaij_I(I, 9, 9, sh)*wem(I, 2, 9, sh, mb, ml, scale, mc)
+        e[2,2] = 8 * ast    * k    * sigmaij_I(I, 9, 9, sh)*wem(I, 2, 2, sh, mb, ml, scale, mc)
         e[1,1] = 16/9. * e[2,2]
         e[10,10] = e[9,9]
         e[1,2] = 8/3. * e[2,2]
         e[1,7] = 4/3. * e[2,7]
         e[1,9] = 4/3. * e[2,9]
-        e[2,7] = 8 * ast    * k    * sigmaij_I(I, 7, 9, sh)*wem(I, 2, 7, sh, mb, ml, scale)
+        e[2,7] = 8 * ast    * k    * sigmaij_I(I, 7, 9, sh)*wem(I, 2, 7, sh, mb, ml, scale, mc)
     elif I == 'A':
-        e[7,10] = 8 * ast    * k    * sigmaij_I(I, 7, 10, sh)*wem(I, 7, 10, sh, mb, ml, scale)
-        e[9,10] = 8 * ast    * k    * sigmaij_I(I, 9, 10, sh)*wem(I, 9, 10, sh, mb, ml, scale)
-        e[2,10] = 8 * ast    * k    * sigmaij_I(I, 9, 10, sh)*wem(I, 2, 10, sh, mb, ml, scale)
+        e[7,10] = 8 * ast    * k    * sigmaij_I(I, 7, 10, sh)*wem(I, 7, 10, sh, mb, ml, scale, mc)
+        e[9,10] = 8 * ast    * k    * sigmaij_I(I, 9, 10, sh)*wem(I, 9, 10, sh, mb, ml, scale, mc)
+        e[2,10] = 8 * ast    * k    * sigmaij_I(I, 9, 10, sh)*wem(I, 2, 10, sh, mb, ml, scale, mc)
         e[1,10] = 4/3. *  e[2,10]
     else:
         raise ValueError("I should be 'T', 'L', 'BR', or 'A'")
@@ -242,12 +345,12 @@ def sigmaij_I(I, i, j, sh):
         return sigma79_L(sh) + sigma79_T(sh)
     elif I=='BR' and i==9 and j==9:
         return sigma99_L(sh) + sigma99_T(sh)
-    elif I=='A' and i==7 and j==7:
-        return sigma77_A(sh)
-    elif I=='A' and i==7 and j==9:
-        return sigma79_A(sh)
-    elif I=='A' and i==9 and j==9:
-        return sigma99_A(sh)
+    elif I=='A' and i==7 and j==10:
+        return sigma710_A(sh)
+    elif I=='A' and i==9 and j==10:
+        return sigma910_A(sh)
+    else:
+        raise ValueError("Function sigmaij_I not defined for I, i, j = " +  str((I,i,j)))
 
 def f_u(alpha_e, alpha_s, alpha_s_mu0, mb, l1, l2):
     # semileptonic phase space factor for B->Xulnu
@@ -291,6 +394,32 @@ def bxll_dbrdq2_func(q, lep):
         return bxll_dbrdq2(q2, wc_obj, par, q, lep)
     return fct
 
+def bxll_afb_num_int(q2min, q2max, wc_obj, par, q, lep, **kwargs):
+    def obs(q2):
+        return bxll_afb_num(q2, wc_obj, par, q, lep, **kwargs)
+    return flavio.math.integrate.nintegrate(obs, q2min, q2max)
+
+def bxll_afb_den_int(q2min, q2max, wc_obj, par, q, lep, **kwargs):
+    def obs(q2):
+        return bxll_afb_den(q2, wc_obj, par, q, lep, **kwargs)
+    return flavio.math.integrate.nintegrate(obs, q2min, q2max)
+
+
+def bxll_afb_int_func(q, lep):
+    def fct(wc_obj, par, q2min, q2max):
+        num = bxll_afb_num_int(q2min, q2max, wc_obj, par, q, lep)
+        if num == 0:
+            return 0
+        den = bxll_afb_den_int(q2min, q2max, wc_obj, par, q, lep)
+        return num/den
+    return fct
+
+def bxll_afb_func(q, lep):
+    def fct(wc_obj, par, q2):
+        return bxll_afb(q2, wc_obj, par, q, lep)
+    return fct
+
+
 # Observable instances
 
 _tex = {'e': 'e', 'mu': '\mu', 'tau': r'\tau', 'l': r'\ell'}
@@ -315,6 +444,25 @@ for l in ['e', 'mu', 'tau', 'l']:
         _obs.tex = r"$\frac{d\text{BR}}{dq^2}(" + _process_tex + r")$"
         _obs.add_taxonomy(_process_taxonomy)
         Prediction(_obs_name, bxll_dbrdq2_func(q, l))
+
+        if l != 'tau': # AFB not yet implemented for tau! (ml=0)
+
+            # binned AFB
+            _obs_name = "<AFB>(B->X"+q+l+l+")"
+            _obs = Observable(name=_obs_name, arguments=['q2min', 'q2max'])
+            _obs.set_description(r"Binned normalized forward-backward asymmetry of $" + _process_tex + r"$")
+            _obs.tex = r"$\langle A_\text{FB} \rangle(" + _process_tex + r")$"
+            _obs.add_taxonomy(_process_taxonomy)
+            Prediction(_obs_name, bxll_afb_int_func(q, l))
+
+            # differential AFB
+            _obs_name = "AFB(B->X"+q+l+l+")"
+            _obs = Observable(name=_obs_name, arguments=['q2'])
+            _obs.set_description(r"Normalized forward-backward asymmetry of $" + _process_tex + r"$")
+            _obs.tex = r"$A_\text{FB}(" + _process_tex + r")$"
+            _obs.add_taxonomy(_process_taxonomy)
+            Prediction(_obs_name, bxll_afb_func(q, l))
+
 
 for l in [('mu','e'), ('tau','mu'),]:
     for q in ['s', 'd']:
--- flavio/physics/bdecays/test_bxll.py
+++ flavio/physics/bdecays/test_bxll.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 import flavio
-from flavio.physics.bdecays.bxll import _bxll_dbrdq2
+from flavio.physics.bdecays.bxll import _bxll_dbrdq2, bxll_afb_num_int, bxll_afb_den_int
 
 class TestBXll(unittest.TestCase):
     def test_bxll(self):
@@ -28,7 +28,7 @@ class TestBXll(unittest.TestCase):
         Vcb = flavio.physics.ckm.get_ckm(par)[1,2]
         r = abs(xi_t)**2/Vcb**2/0.9621*0.574/par['C_BXlnu']*par['BR(B->Xcenu)_exp']/0.1051
         self.assertAlmostEqual(1e6*flavio.sm_prediction('<BR>(B->Xsmumu)', 1, 3.5)/r,
-                               0.888, delta=0.03)
+                               0.888, delta=0.02)
         self.assertAlmostEqual(1e6*flavio.sm_prediction('<BR>(B->Xsmumu)', 3.5, 6)/r,
                                0.731, delta=0.01)
         self.assertAlmostEqual(1e7*flavio.sm_prediction('<BR>(B->Xsmumu)', 14.4, 25)/r,
@@ -43,8 +43,50 @@ class TestBXll(unittest.TestCase):
     def test_bxll_lratio(self):
         # compare to arXiv:1503.04849
         self.assertAlmostEqual(flavio.sm_prediction('<Rmue>(B->Xsll)', 1, 3.5),
-                               0.96, delta=0.02)
+                               0.96, delta=0.01)
         self.assertAlmostEqual(flavio.sm_prediction('<Rmue>(B->Xsll)', 14.4, 25),
-                               1.15, delta=0.02)
+                               1.15, delta=0.01)
         # for tau, just check this doesn't raise an error
         flavio.sm_prediction('<Rtaumu>(B->Xsll)', 14.4, 25)
+
+    def test_bxll_afb(self):
+        # check calling outside of kinematical regions yields 0
+        self.assertAlmostEqual(flavio.sm_prediction('AFB(B->Xsmumu)', 0), 0)
+        self.assertAlmostEqual(flavio.sm_prediction('AFB(B->Xsll)', 30), 0)
+        # just check differential AFB doesn't raise errors
+        flavio.sm_prediction('AFB(B->Xsee)', 1)
+        flavio.sm_prediction('AFB(B->Xsmumu)', 6)
+        flavio.sm_prediction('AFB(B->Xsll)', 14.4)
+        # check whether QED corrections have the right behaviour
+        # (table 2 of arXiv:1503.04849)
+        wc_obj = flavio.WilsonCoefficients()
+        par = flavio.default_parameters.get_central_all()
+        afb_num_low1_noqed = bxll_afb_num_int(1, 3.5, wc_obj, par, 's', 'e', include_qed=False)
+        afb_num_low1_qed   = bxll_afb_num_int(1, 3.5, wc_obj, par, 's', 'e', include_qed=True)
+        afb_num_low2_noqed = bxll_afb_num_int(3.5, 6, wc_obj, par, 's', 'e', include_qed=False)
+        afb_num_low2_qed   = bxll_afb_num_int(3.5, 6, wc_obj, par, 's', 'e', include_qed=True)
+        afb_den_low1_noqed = bxll_afb_den_int(1, 3.5, wc_obj, par, 's', 'e', include_qed=False)
+        afb_den_low1_qed   = bxll_afb_den_int(1, 3.5, wc_obj, par, 's', 'e', include_qed=True)
+        afb_den_low2_noqed = bxll_afb_den_int(3.5, 6, wc_obj, par, 's', 'e', include_qed=False)
+        afb_den_low2_qed   = bxll_afb_den_int(3.5, 6, wc_obj, par, 's', 'e', include_qed=True)
+        self.assertAlmostEqual((afb_num_low1_qed-afb_num_low1_noqed)/afb_num_low1_qed,
+                                -0.107, delta=0.050) # should lead to a -10.7% suppression
+        self.assertAlmostEqual((afb_num_low2_qed-afb_num_low2_noqed)/afb_num_low2_qed,
+                                +0.162, delta=0.020) # should lead to a 16.2% enhancement
+        self.assertAlmostEqual((afb_den_low1_qed-afb_den_low1_noqed)/afb_den_low1_qed,
+                                0.068, delta=0.005) # should lead to a 6.8% enhancement
+        self.assertAlmostEqual((afb_den_low2_qed-afb_den_low2_noqed)/afb_den_low2_qed,
+                                0.031, delta=0.010) # should lead to a 3.1% enhancement
+        # compare SM predictions to arXiv:1503.04849
+        self.assertAlmostEqual(
+            flavio.sm_prediction('<AFB>(B->Xsee)', 1, 3.5)/(3/4.*(-1.03e-7)/((2.91e-7)+(6.35e-7))),
+            1, delta=0.15)
+        self.assertAlmostEqual(
+            flavio.sm_prediction('<AFB>(B->Xsee)', 3.5, 6)/(3/4.*(0.73e-7)/((2.43e-7)+(4.97e-7))),
+            1, delta=0.1)
+        self.assertAlmostEqual(
+            flavio.sm_prediction('<AFB>(B->Xsmumu)', 1, 3.5)/(3/4.*(-1.10e-7)/((2.09e-7)+(6.79e-7))),
+            1, delta=0.1)
+        self.assertAlmostEqual(
+            flavio.sm_prediction('<AFB>(B->Xsmumu)', 3.5, 6)/(3/4.*(0.67e-7)/((1.94e-7)+(5.34e-7))),
+            1, delta=0.1)
