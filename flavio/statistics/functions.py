4d606693cc6eb100273183b3f6a71d8964d56c12
David Straub <david.straub@tum.de>
2017-01-31 10:35:25 +0100

    [statistics] Simple function to get CL from no. of sigmas

--- flavio/statistics/functions.py
+++ flavio/statistics/functions.py
@@ -1,4 +1,14 @@
 import scipy.stats
+from functools import lru_cache
+
+@lru_cache(maxsize=10)
+def confidence_level(nsigma):
+    r"""Return the confidence level corresponding to a number of sigmas,
+    i.e. the probability contained in the normal distribution between $-n\sigma$
+    and $+n\sigma$.
+
+    Example: `confidence_level(1)` returns approximately 0.68."""
+    return (scipy.stats.norm.cdf(nsigma)-0.5)*2
 
 def delta_chi2(nsigma, dof):
     r"""Compute the $\Delta\chi^2$ for `dof` degrees of freedom corresponding
@@ -6,8 +16,8 @@ def delta_chi2(nsigma, dof):
 
     Example: For `dof=2` and `nsigma=1`, the result is roughly 2.3."""
     if dof == 1:
+        # that's trivial
         return nsigma**2
-    chi2_1dof = scipy.stats.chi2(1)
     chi2_ndof = scipy.stats.chi2(dof)
-    cl_nsigma = chi2_1dof.cdf(nsigma**2) # this is roughly 0.68 for n_sigma=1 etc.
+    cl_nsigma = confidence_level(nsigma)
     return chi2_ndof.ppf(cl_nsigma)
--- flavio/statistics/test_functions.py
+++ flavio/statistics/test_functions.py
@@ -9,3 +9,8 @@ class TestFunctions(unittest.TestCase):
         self.assertAlmostEqual(delta_chi2(1, 2), 2.30, delta=0.006)
         self.assertAlmostEqual(delta_chi2(2, 3), 8.03, delta=0.006)
         self.assertAlmostEqual(delta_chi2(3, 2), 11.83, delta=0.006)
+
+    def test_cl(self):
+        self.assertAlmostEqual(confidence_level(1), 0.6826894921370859, places=10)
+        self.assertAlmostEqual(confidence_level(2), 0.9544997361036416, places=10)
+        self.assertAlmostEqual(confidence_level(5), 0.9999994266968562, places=10)
