9fdd1b32bfde7aac4fcebb995267e1244d4fb275
Peter Stangl <peterstangl@users.noreply.github.com>
2018-04-25 16:32:43 +0200

    fix issue with Computer Modern Roman font (issue #44) (#45)

--- flavio/plots/config.py
+++ flavio/plots/config.py
@@ -3,7 +3,8 @@
 from matplotlib import rc, rcParams
 
 rc('text', usetex=True)
-rc('font',**{'family':'serif','serif':['Computer Modern Roman']})
+rc('font',**{'family':'serif',
+             'serif':['Computer Modern Roman','cmu serif']+rcParams['font.serif']})
 rcParams['xtick.labelsize'] = 10
 rcParams['ytick.labelsize'] = 10
 rcParams['axes.labelsize'] = 14
