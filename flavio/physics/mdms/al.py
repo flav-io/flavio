4dfc0d3b92fb979567a0536a81c72cf7d0da8d42
David Straub <david.straub@tum.de>
2018-09-11 13:37:00 +0200

    [mdm] Correct the sign of the lepton MDM

--- flavio/physics/mdms/al.py
+++ flavio/physics/mdms/al.py
@@ -8,13 +8,14 @@ def al(wc_obj, par, l, scale):
     r"""Anomalous magnetic moment of lepton `l` at scale `scale`,
     $a_\ell = (g_\ell - 2) / 2$."""
     wc = wc_obj.get_wc(sector='dF=0', scale=scale, par=par, eft='WET-3', basis='flavio')
-    # the Lagrangian contains -e*a/(4*m) (...)
+    # the covariant derivative contains d_mu + eta i e Q A, where Q=-1
+    # the Lagrangian contains -eta * e*Q*a/(4*m) (...)
     # the effective Lagrangian contains p * ReC7 (...)
     # with p = 4*GF/sqrt(2)*e/(16*pi**2)*m
-    # thus a = -4*m*p/e * ReC7 = pre * ReC7
+    # thus a = -eta*4*m*p/e/Q * ReC7 = pre * ReC7
     GF = par['GF']
     m = par['m_' + l]
-    pre = -m**2 * GF / sqrt(2) / pi**2
+    pre = m**2 * GF / sqrt(2) / pi**2
     C7 = wc['C7_' + 2 * l]  # e.g. C7_mumu
     aSM = par['a_{} SM'.format(l)]  # e.g. a_mu SM
     return aSM + pre * C7.real
--- flavio/physics/mdms/test_al.py
+++ flavio/physics/mdms/test_al.py
@@ -7,6 +7,7 @@ from math import sqrt, pi
 par = flavio.default_parameters.get_central_all()
 aSM = 11659182.3e-10
 
+
 class TestGminus2Leptons(unittest.TestCase):
     def test_amu_SM(self):
         self.assertAlmostEqual(flavio.sm_prediction('a_mu') / aSM, 1)
@@ -16,7 +17,7 @@ class TestGminus2Leptons(unittest.TestCase):
         e = sqrt(4 * pi * par['alpha_e'])
         m = par['m_mu']
         p = 4 * par['GF'] / sqrt(2) * e / 16 / pi**2 * m
-        pre = -p * 4 * m / e
+        pre = p * 4 * m / e
         a = pre * 1e-3
         self.assertAlmostEqual(flavio.np_prediction('a_mu', w) - aSM,
                                a,
