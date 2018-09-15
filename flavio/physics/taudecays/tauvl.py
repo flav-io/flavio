4661fe89095ad8e01423fbcb0b4264ed564953d4
Peter Stangl <peter.stangl@ph.tum.de>
2018-09-15 02:13:20 +0200

    fix signs (cov.deriv. convention and outgoing momentum of meson)

--- flavio/physics/taudecays/tauvl.py
+++ flavio/physics/taudecays/tauvl.py
@@ -39,16 +39,16 @@ def br_tauvl(wc_obj, par, V, lep):
         g_u = get_wcs(wc, 'u', lep)
         g_d = get_wcs(wc, 'd', lep)
         g = (g_u-g_d)/sqrt(2)
-        KV = 1/sqrt(2)*e
+        KV = -1/sqrt(2)*e
     if V == 'phi':
         g = get_wcs(wc, 's', lep)
-        KV = -1/3*e
+        KV = 1/3*e
     gL = mV*fV/2 * (g[0] + g[1])
     gR = mV*fV/2 * (g[2] + g[3])
-    gTL  =  fTV * g[4].conjugate() + 2*fV*KV/mV * Cgamma_ltau.conjugate()
-    gtTL = -fTV * g[4].conjugate()
-    gTR  =  fTV * g[5] + 2*fV*KV/mV * Cgamma_taul
-    gtTR =  fTV * g[5]
+    gTL  = -fTV * g[4].conjugate() - 2*fV*KV/mV * Cgamma_ltau.conjugate()
+    gtTL = +fTV * g[4].conjugate()
+    gTR  = -fTV * g[5] - 2*fV*KV/mV * Cgamma_taul
+    gtTR = -fTV * g[5]
     return (par['tau_tau']
             * common.GammaFvf(mtau, mV, ml, gL, gR, gTL, gtTL, gTR, gtTR) )
 
