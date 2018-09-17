21a6b9fbc9473e2e675ec253f0be8fc67f7dfb88
David Straub <david.straub@tum.de>
2018-09-17 18:15:40 +0200

    [likelihood] Small fix (#6)
    Revert "[measurements] Add LEP & SLD measurements of EWPOs"
    
    This reverts commit 658e45786e7105c9c3e1e3499ca1d1baf268348d.

--- flavio/statistics/likelihood.py
+++ flavio/statistics/likelihood.py
@@ -443,7 +443,7 @@ class MeasurementCovariance(object):
             covariances.append(covariance)
         # if there is only a single measuement
         if len(means) == 1:
-            return means[0][0], covariances[0]
+            return means[0], covariances[0]
         # if there are severeal measurements, perform a weighted average
         else:
             # covariances: [Sigma_1, Sigma_2, ...]
