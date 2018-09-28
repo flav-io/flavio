173fb2930ed8db6bc49143c2512ae8a98216390a
David Straub <david.straub@tum.de>
2018-09-28 10:43:50 +0200

    [setup] Bump wilson dependency to 1.2.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(name='flavio',
                 ]
       },
       install_requires=['numpy', 'scipy>=0.18', 'setuptools>=3.3', 'pyyaml',
-                        'wcxf>=1.4.4', 'ckmutil', 'wilson>=1.2', ],
+                        'wcxf>=1.4.4', 'ckmutil', 'wilson>=1.2.1', ],
       extras_require={
             'testing': ['nose'],
             'plotting': ['matplotlib>=1.4'],
