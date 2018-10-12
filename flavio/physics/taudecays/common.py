2ea5e5fc02896b1e0d1c7e9ecb0d089e27ce818d
Peter Stangl <peter.stangl@ph.tum.de>
2018-10-12 12:13:20 +0200

    extend GammaFvf for decay to massless vector boson

--- flavio/physics/taudecays/common.py
+++ flavio/physics/taudecays/common.py
@@ -5,20 +5,48 @@ from math import sqrt, pi
 
 def GammaFvf(M, mv, mf, gL, gR, gTL=0, gtTL=0, gTR=0, gtTR=0):
     """Generic decay width of a fermion to a vector boson and a fermion."""
-    amp2_V = ((abs(gL)**2 + abs(gR)**2)
-            * ((mf**2 - M**2)**2 /mv**2 + mf**2 + M**2 - 2. *mv**2)
-            - 12 * mf * M * ( gR*gL.conjugate() ).real )/2.
-    if any([gTL,gtTL,gTR,gtTR]):
-        amp2_T = mv**2/2*( (abs(gTL-gtTL)**2 + abs(gTR+gtTR)**2)
-                * (2*(mf**2 - M**2)**2 /mv**2 - mf**2 - M**2 - mv**2)
-                - 12 * mf * M * ( (gTR+gtTR)*(gTL-gtTL).conjugate() ).real )
-        I = ( 3*M * (M**2 - mf**2 - mv**2)
-            *( gL*(gTR+gtTR).conjugate() + gR*(gTL-gtTL).conjugate() ).real
-            + 3*mf * (mf**2 - M**2 - mv**2)
-            *( gR*(gTR+gtTR).conjugate() + gL*(gTL-gtTL).conjugate() ).real )
-        amp2 = amp2_V + amp2_T + I
+    if mv == 0:
+        if any([gL,gR]):
+            amp2_V = ((abs(gL)**2 + abs(gR)**2) * (M**2 + mf**2)
+                    - 8 * mf * M * ( gR*gL.conjugate() ).real)
+        else:
+            amp2_V = False
+        if any([gTL,gtTL,gTR,gtTR]):
+            amp2_T = ( (M**2 - mf**2)**2 * (
+                        abs(gTL)**2 + abs(gTR)**2
+                      + abs(gtTL)**2 + abs(gtTR)**2
+                    - 2 *( (gTL*gtTL.conjugate()).real
+                         - (gTR*gtTR.conjugate()).real ) ))
+        else:
+            amp2_T = False
+        if amp2_V is not False and amp2_T is not False:
+            I = ( 3*M * (M**2 - mf**2)
+                *( gL*(gTR+gtTR).conjugate() + gR*(gTL-gtTL).conjugate() ).real
+                + 3*mf * (mf**2 - M**2)
+                *( gR*(gTR+gtTR).conjugate() + gL*(gTL-gtTL).conjugate() ).real )
+        else:
+            I = 0
     else:
-        amp2 = amp2_V
+        if any([gL,gR]):
+            amp2_V = ((abs(gL)**2 + abs(gR)**2)
+                    * ((mf**2 - M**2)**2 /mv**2 + mf**2 + M**2 - 2. *mv**2)
+                    - 12 * mf * M * ( gR*gL.conjugate() ).real )/2.
+        else:
+            amp2_V = False
+        if any([gTL,gtTL,gTR,gtTR]):
+            amp2_T = mv**2/2*( (abs(gTL-gtTL)**2 + abs(gTR+gtTR)**2)
+                    * (2*(mf**2 - M**2)**2 /mv**2 - mf**2 - M**2 - mv**2)
+                    - 12 * mf * M * ( (gTR+gtTR)*(gTL-gtTL).conjugate() ).real )
+        else:
+           amp2_T = False
+        if amp2_V is not False and amp2_T is not False:
+            I = ( 3*M * (M**2 - mf**2 - mv**2)
+                *( gL*(gTR+gtTR).conjugate() + gR*(gTL-gtTL).conjugate() ).real
+                + 3*mf * (mf**2 - M**2 - mv**2)
+                *( gR*(gTR+gtTR).conjugate() + gL*(gTL-gtTL).conjugate() ).real )
+        else:
+            I = 0
+    amp2 = float(amp2_V) + float(amp2_T) + I
     return Gamma(amp2, M, mf, mv)
 
 def GammaFsf(M, ms, mf, gL, gR):
