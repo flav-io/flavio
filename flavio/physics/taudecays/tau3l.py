3778832a97cc293d37c428786687f17e0dc46e3d
David Straub <david.straub@tum.de>
2018-11-08 16:38:38 +0100

    [tau3l] Add tau->emue and tau->muemu

--- flavio/physics/taudecays/tau3l.py
+++ flavio/physics/taudecays/tau3l.py
@@ -1,4 +1,4 @@
-r"""Functions for lepton flavour violating $\tau\to \ell_1\ell_2\ell_2$ decays."""
+r"""Functions for lepton flavour violating $\tau\to \ell_1\ell_2\ell_3$ decays."""
 
 import flavio
 from math import pi, log, sqrt
@@ -7,8 +7,9 @@ from math import pi, log, sqrt
 # names of LFV sectors in WCxf
 wcxf_sector_names = {('tau', 'mu'): 'mutau',
                      ('tau', 'e'): 'taue',
-                     ('mu', 'e'): 'mue', }
-
+                     ('mu', 'e'): 'mue',
+                     ('tau', 'e', 'mu', 'e'): 'etauemu',
+                     ('tau', 'mu', 'e', 'mu'): 'muemutau', }
 
 
 def _BR_taumuee(mtau, me, wc):
@@ -35,8 +36,7 @@ def _BR_tau3mu(mtau, mmu, wc):
 def _BR_taumuemu(wc):
     r"""Function for $\Delta L=2$ decays like $\tau^-\to \mu^- e^+ \mu^-$."""
     return (abs(wc['CVLL'])**2 + abs(wc['CVLR'])**2 + abs(wc['CVRL'])**2 + abs(wc['CVRR'])**2
-            + 1 / 4 * (abs(wc['CSLL'])**2 + abs(wc['CSLR'])**2 + abs(wc['CSRL'])**2 + abs(wc['CSRR'])**2)
-            + 12 * (abs(wc['CTLL'])**2 + abs(wc['CTRR'])**2))
+            + 1 / 4 * (abs(wc['CSLL'])**2 + abs(wc['CSRR'])**2))
 
 
 def wc_eff(wc_obj, par, scale, l0, l1, l2, l3, nf_out=4):
@@ -45,7 +45,7 @@ def wc_eff(wc_obj, par, scale, l0, l1, l2, l3, nf_out=4):
     if l2 == l3:
         sector = wcxf_sector_names[l0, l1]
     else:
-        raise ValueError("Not implemented")
+        sector = wcxf_sector_names[l0, l1, l2, l3]
     alpha = flavio.physics.running.running.get_alpha_e(par, scale, nf_out=4)
     e = sqrt(4 * pi * alpha)
     ml0 = par['m_' + l0]
@@ -55,8 +55,8 @@ def wc_eff(wc_obj, par, scale, l0, l1, l2, l3, nf_out=4):
         wceff['C7'] = e / ml0 * wc['Cgamma_taumu']
         wceff['C7p'] = e / ml0 * wc['Cgamma_mutau'].conjugate()
         wceff['CVLL'] = wc['CVLL_mumutaumu']
-        wceff['CVLR'] = wc['CVLR_mumutaumu']
-        wceff['CVRL'] = wc['CVLR_taumumumu']
+        wceff['CVLR'] = wc['CVLR_taumumumu']
+        wceff['CVRL'] = wc['CVLR_mumutaumu']
         wceff['CVRR'] = wc['CVRR_mumutaumu']
         wceff['CSRR'] = wc['CSRR_mumutaumu']
         wceff['CSLL'] = wc['CSRR_mumumutau'].conjugate()
@@ -64,8 +64,8 @@ def wc_eff(wc_obj, par, scale, l0, l1, l2, l3, nf_out=4):
         wceff['C7'] = e / ml0 * wc['Cgamma_taue']
         wceff['C7p'] = e / ml0 * wc['Cgamma_etau'].conjugate()
         wceff['CVLL'] = wc['CVLL_eetaue']
-        wceff['CVLR'] = wc['CVLR_eetaue']
-        wceff['CVRL'] = wc['CVLR_taueee']
+        wceff['CVLR'] = wc['CVLR_taueee']
+        wceff['CVRL'] = wc['CVLR_eetaue']
         wceff['CVRR'] = wc['CVRR_eetaue']
         wceff['CSRR'] = wc['CSRR_eetaue']
         wceff['CSLL'] = wc['CSRR_eeetau'].conjugate()
@@ -73,8 +73,8 @@ def wc_eff(wc_obj, par, scale, l0, l1, l2, l3, nf_out=4):
         wceff['C7'] = e / ml0 * wc['Cgamma_taumu']
         wceff['C7p'] = e / ml0 * wc['Cgamma_mutau'].conjugate()
         wceff['CVLL'] = wc['CVLL_eetaumu']
-        wceff['CVLR'] = wc['CVLR_eetaumu']
-        wceff['CVRL'] = wc['CVLR_taumuee']
+        wceff['CVLR'] = wc['CVLR_taumuee']
+        wceff['CVRL'] = wc['CVLR_eetaumu']
         wceff['CVRR'] = wc['CVRR_eetaumu']
         wceff['CSRR'] = wc['CSRR_eetaumu'] - wc['CSRR_taueemu'] / 2
         wceff['CSLL'] = wc['CSRR_eemutau'].conjugate() - wc['CSRR_mueetau'].conjugate() / 2
@@ -86,8 +86,8 @@ def wc_eff(wc_obj, par, scale, l0, l1, l2, l3, nf_out=4):
         wceff['C7'] = e / ml0 * wc['Cgamma_taue']
         wceff['C7p'] = e / ml0 * wc['Cgamma_etau'].conjugate()
         wceff['CVLL'] = wc['CVLL_muetaumu']
-        wceff['CVLR'] = wc['CVLR_mumutaue']
-        wceff['CVRL'] = wc['CVLR_tauemumu']
+        wceff['CVLR'] = wc['CVLR_tauemumu']
+        wceff['CVRL'] = wc['CVLR_mumutaue']
         wceff['CVRR'] = wc['CVRR_muetaumu']
         wceff['CSRR'] = wc['CSRR_tauemumu'] - wc['CSRR_muetaumu'] / 2
         wceff['CSLL'] = wc['CSRR_mumuetau'].conjugate() - wc['CSRR_emumutau'].conjugate() / 2
@@ -99,11 +99,25 @@ def wc_eff(wc_obj, par, scale, l0, l1, l2, l3, nf_out=4):
         wceff['C7'] = e / ml0 * wc['Cgamma_mue']
         wceff['C7p'] = e / ml0 * wc['Cgamma_emu'].conjugate()
         wceff['CVLL'] = wc['CVLL_eemue']
-        wceff['CVLR'] = wc['CVLR_eemue']
-        wceff['CVRL'] = wc['CVLR_mueee']
+        wceff['CVLR'] = wc['CVLR_mueee']
+        wceff['CVRL'] = wc['CVLR_eemue']
         wceff['CVRR'] = wc['CVRR_eemue']
         wceff['CSRR'] = wc['CSRR_eemue']
         wceff['CSLL'] = wc['CSRR_eeemu'].conjugate()
+    elif (l0, l1, l2, l3) == ('tau', 'e', 'mu', 'e'):
+        wceff['CVLL'] = wc['CVLL_muetaue']
+        wceff['CVLR'] = wc['CVLR_tauemue']
+        wceff['CVRL'] = wc['CVLR_muetaue']
+        wceff['CVRR'] = wc['CVRR_muetaue']
+        wceff['CSRR'] = wc['CSRR_muetaue']
+        wceff['CSLL'] = wc['CSRR_emuetau'].conjugate()
+    elif (l0, l1, l2, l3) == ('tau', 'mu', 'e', 'mu'):
+        wceff['CVLL'] = wc['CVLL_muemutau'].conjugate()
+        wceff['CVLR'] = wc['CVLR_taumuemu']
+        wceff['CVRL'] = wc['CVLR_muemutau'].conjugate()
+        wceff['CVRR'] = wc['CVRR_muemutau'].conjugate()
+        wceff['CSRR'] = wc['CSRR_muemutau'].conjugate()
+        wceff['CSLL'] = wc['CSRR_emutaumu']
     else:
         raise ValueError("Decay {}-->{}-{}+{}- not implemented".format(l0, l1, l2, l3))
     return wceff
@@ -130,7 +144,6 @@ def BR_taul1l2l3(wc_obj, par, l1, l2, l3):
     return pre_br * br_wc
 
 
-
 # function returning function needed for prediction instance
 def br_taul1l2l3_fct(l1, l2, l3):
     def f(wc_obj, par):
@@ -142,11 +155,12 @@ def br_taul1l2l3_fct(l1, l2, l3):
 _tex = {'e': 'e', 'mu': r'\mu'}
 
 for (l1, l2, l3) in [('mu', 'e', 'e'), ('mu', 'mu', 'mu'),
-                     ('e', 'e', 'e'), ('e', 'mu', 'mu')]:
-    _process_tex = r"\tau^-\to " + _tex[l1] + r"^-" + _tex[l2] + r"^+" + _tex[l2] + r"^-"
+                     ('e', 'e', 'e'), ('e', 'mu', 'mu'),
+                     ('e', 'mu', 'e'), ('mu', 'e', 'mu')]:
+    _process_tex = r"\tau^-\to " + _tex[l1] + r"^-" + _tex[l2] + r"^+" + _tex[l3] + r"^-"
     _process_taxonomy = r'Process :: $\tau$ lepton decays :: LFV decays :: $\tau\to \ell^\prime\ell\ell$ :: $' + _process_tex + r"$"
 
-    _obs_name = "BR(tau->" + l1 + 2 * l2 + ")"
+    _obs_name = "BR(tau->" + l1 + l2 + l3 + ")"
     _obs = flavio.classes.Observable(_obs_name)
     _obs.set_description(r"Branching ratio of $" + _process_tex + r"$")
     _obs.tex = r"$\text{BR}(" + _process_tex + r")$"
--- flavio/physics/taudecays/test_tau3l.py
+++ flavio/physics/taudecays/test_tau3l.py
@@ -3,9 +3,10 @@ import flavio
 
 
 class TestTau3l(unittest.TestCase):
-    def test_tau3l(self):
-        # compare to the experimental values
+    def test_tau3l_sm(self):
         self.assertEqual(flavio.sm_prediction('BR(tau->muee)'), 0)
         self.assertEqual(flavio.sm_prediction('BR(tau->mumumu)'), 0)
         self.assertEqual(flavio.sm_prediction('BR(tau->emumu)'), 0)
         self.assertEqual(flavio.sm_prediction('BR(tau->eee)'), 0)
+        self.assertEqual(flavio.sm_prediction('BR(tau->emue)'), 0)
+        self.assertEqual(flavio.sm_prediction('BR(tau->muemu)'), 0)
