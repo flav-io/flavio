8c662caa2374a8ae53f55cd2fc3d2583fb9b64cc
David Straub <david.straub@tum.de>
2018-12-21 14:39:14 +0100

    Make test deterministic

--- flavio/physics/mdms/test_al.py
+++ flavio/physics/mdms/test_al.py
@@ -2,6 +2,7 @@ import unittest
 import flavio
 from wilson import Wilson
 from math import sqrt, pi
+import numpy as np
 
 
 par = flavio.default_parameters.get_central_all()
@@ -33,6 +34,7 @@ class TestGminus2Leptons(unittest.TestCase):
         pd = flavio.combine_measurements('a_e')
         ae_exp = pd.central_value
         ae_err_exp = pd.error_left
+        np.random.seed(17)
         ae_err_sm = flavio.sm_uncertainty('a_e')
         # check that there is a -2.3 sigma tension, see 1804.07409 p. 13
         self.assertAlmostEqual((ae_exp - ae_SM) / sqrt(ae_err_sm**2 + ae_err_exp**2), -2.3, delta=0.5)
