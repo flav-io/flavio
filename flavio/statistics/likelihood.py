57f97bb39628eb9b924234ce447a15315eb6cec3
Peter Stangl <peter.stangl@ph.tum.de>
2019-03-15 17:10:37 +0100

    cache only values of last call

--- flavio/statistics/likelihood.py
+++ flavio/statistics/likelihood.py
@@ -60,7 +60,7 @@ class MeasurementLikelihood(iio.YAMLLoadable):
         self.exclude_measurements = exclude_measurements
         self.include_measurements = include_measurements
         self.include_pseudo_measurements = include_pseudo_measurements
-        self.predictions_cache = {}
+        self.predictions_cache = (None, None)
         if exclude_measurements and include_measurements:
             raise ValueError("The options exclude_measurements and include_measurements must not be specified simultaneously")
 
@@ -139,15 +139,15 @@ class MeasurementLikelihood(iio.YAMLLoadable):
         a parameter dictionary `par_dict`and WilsonCoefficient instance
         `wc_obj`"""
         predictions_key = (hash(frozenset(par_dict.items())),hash(wc_obj))
-        if predictions_key not in self.predictions_cache:
+        if predictions_key != self.predictions_cache[0]:
             all_predictions = {}
             for observable in self.observables:
                 obs = flavio.classes.Observable.argument_format(observable, 'dict')
                 name = obs.pop('name')
                 _inst = flavio.classes.Observable[name]
                 all_predictions[observable] = _inst.prediction_par(par_dict, wc_obj, **obs)
-            self.predictions_cache[predictions_key] = all_predictions
-        return self.predictions_cache[predictions_key]
+            self.predictions_cache = (predictions_key, all_predictions)
+        return self.predictions_cache[1]
 
     def log_likelihood_pred(self, pred_dict):
         """Return the logarithm of the likelihood function as a function of
