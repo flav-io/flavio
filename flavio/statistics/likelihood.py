7b08a5c617700f585686fc261484699bc7ea8887
Peter Stangl <peter.stangl@ph.tum.de>
2019-03-15 18:09:05 +0100

    use separate attributes for hash and values in cache, make them private

--- flavio/statistics/likelihood.py
+++ flavio/statistics/likelihood.py
@@ -60,7 +60,8 @@ class MeasurementLikelihood(iio.YAMLLoadable):
         self.exclude_measurements = exclude_measurements
         self.include_measurements = include_measurements
         self.include_pseudo_measurements = include_pseudo_measurements
-        self.predictions_cache = (None, None)
+        self._predictions_cache_hash = None
+        self._predictions_cache_values = None
         if exclude_measurements and include_measurements:
             raise ValueError("The options exclude_measurements and include_measurements must not be specified simultaneously")
 
@@ -143,15 +144,16 @@ class MeasurementLikelihood(iio.YAMLLoadable):
         """
         # compute a hash from the function's arguments used for caching
         arg_hash = hash((frozenset(par_dict.items()),wc_obj))
-        if arg_hash != self.predictions_cache[0]:
+        if arg_hash != self._predictions_cache_hash:
             all_predictions = {}
             for observable in self.observables:
                 obs = flavio.classes.Observable.argument_format(observable, 'dict')
                 name = obs.pop('name')
                 _inst = flavio.classes.Observable[name]
                 all_predictions[observable] = _inst.prediction_par(par_dict, wc_obj, **obs)
-            self.predictions_cache = (arg_hash, all_predictions)
-        return self.predictions_cache[1]
+            self._predictions_cache_values = all_predictions
+            self._predictions_cache_hash = arg_hash
+        return self._predictions_cache_values
 
     def log_likelihood_pred(self, pred_dict):
         """Return the logarithm of the likelihood function as a function of
