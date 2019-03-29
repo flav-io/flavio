7d821e4d97684ea112a44a7e8782198a6c20f359
Broko55 <jonathan.kriewald@udo.edu>
2019-03-29 17:43:01 +0100

    Added test script and current upper limit by SINDRUM II for Gold Target

---
+++ flavio/physics/mudecays/test_mueconversion.py
@@ -0,0 +1,7 @@
+import unittest
+import flavio
+
+class TestMuEConversion(unittest.TestCase):
+	def test_mueconversion(self):
+		self.assertEqual(flavio.sm_prediction('CR(mu->e, Au'), 0)
+		self.assertEqual(flavio.sm_prediction('CR(mu->e, Al'), 0)
