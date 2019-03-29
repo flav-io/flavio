54cb0eff4a2b57bf2580e506e74eb73db60faddb
Broko55 <jonathan.kriewald@udo.edu>
2019-03-29 18:16:25 +0100

    Fixing the really embarassing Identation errors, I'm really sorry

--- flavio/physics/mudecays/test_mueconversion.py
+++ flavio/physics/mudecays/test_mueconversion.py
@@ -3,5 +3,5 @@ import flavio
 
 class TestMuEConversion(unittest.TestCase):
 	def test_mueconversion(self):
-		self.assertEqual(flavio.sm_prediction('CR(mu->e, Au'), 0)
-		self.assertEqual(flavio.sm_prediction('CR(mu->e, Al'), 0)
+		self.assertEqual(flavio.sm_prediction('CR(mu->e, Au)'), 0)
+		self.assertEqual(flavio.sm_prediction('CR(mu->e, Al)'), 0)
