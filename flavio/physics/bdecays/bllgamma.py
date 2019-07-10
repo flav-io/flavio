d56c18f5fa2526c99389fa199757b0631237fd3c
mreboud <meril.reboud@cern.ch>
2019-07-10 11:47:14 +0200

    Correct typo, use the correct wc, un-hardcode Bs and a1

--- flavio/physics/bdecays/bllgamma.py
+++ flavio/physics/bdecays/bllgamma.py
@@ -24,12 +24,14 @@ def _Co(z):
 # hep-ph/0410146:
 #   - Uses the C7>0, C9<0, C10>0 convention
 #   - has a global minus sign typo in dG12dsMN
-#   - wrongly defines f_phi (up to a -3 factor)
+#   - "wrongly" defines f_phi (-3 factor wrt 1501.06569)
+#   - Defines an effective C9 (as for B->P/Vll), for the moment C9 is used,
+#     which means that charmonium is not implemented!!
 ##################
 
 # form factors
 def get_ff(q2, par, B):
-    return AuxiliaryQuantity['Bs->gamma form factor'].prediction(par_dict=par, wc_obj=None, q2=q2, B=B)
+    return AuxiliaryQuantity['B->gamma form factor'].prediction(par_dict=par, wc_obj=None, q2=q2, B=B)
 
 
 def prefactor(s, par, B, ff, lep, wc):
@@ -40,36 +42,50 @@ def prefactor(s, par, B, ff, lep, wc):
     xi_t = ckm.xi('t',bq)(par)
     return GF**2/(2**10*pi**4)*abs(xi_t)**2*alphaem**3*par['m_'+B]**5
 
+def getfftv(s, par, B, ff, ff0, lep, wc):
+    scale = config['renormalization scale']['bllgamma']
+    mb = running.get_mb(par, scale, nf_out=5)
+    gphi = -par['Bs->phi BSZ a0_T1']
+    fphi = -1./3*par['f_phi']
+    bq = meson_quark[B]
+    a1 = -wc['C1_'+bq]-wc['C2_'+bq]/3 #minus sign comes from WC sign convention
+    fftv = ff['tv']+ff0['tv']-2*fphi*gphi*par['m_'+B]**2*s/par['m_phi']/(par['m_'+B]**2*s-par['m_phi']**2+1j*par['m_phi']/par['tau_phi'])+16/3*a1/wc['C7_'+bq]*par['f_'+B]/mb
+    return fftv
+    
+def getffta(s, par, B, ff, ff0, lep, wc):
+    scale = config['renormalization scale']['bllgamma']
+    gphi = -par['Bs->phi BSZ a0_T1']
+    fphi = -1./3*par['f_phi']
+    ffta = ff['ta']+ff0['ta']-2*fphi*gphi*par['m_'+B]**2*s/par['m_phi']/(par['m_'+B]**2*s-par['m_phi']**2+1j*par['m_phi']/par['tau_phi'])
+    return ffta
+
 def getF1(s, par, B, ff, ff0, lep, wc):
     scale = config['renormalization scale']['bllgamma']
     mb = running.get_mb(par, scale, nf_out=5)
     mbh = mb/par['m_'+B]
-    gphi = -par['Bs->phi BSZ a0_T1']
-    fphi = -1./3*par['f_phi']
-    #WILL MOVE TO par
-    a1 = -0.13 #-(C1+C2/3)
-    fftv = ff['tv']+ff0['tv']-2*fphi*gphi*par['m_'+B]**2*s/par['m_phi']/(par['m_'+B]**2*s-par['m_phi']**2+1j*par['m_phi']/par['tau_phi'])+16/3*a1/wc['7']*par['f_'+B]/mb
-    ffta = ff['ta']+ff0['ta']-2*fphi*gphi*par['m_'+B]**2*s/par['m_phi']/(par['m_'+B]**2*s-par['m_phi']**2+1j*par['m_phi']/par['tau_phi'])
-    return (abs(wc['v'])**2 + abs(wc['a'])**2)*ff['v']**2 + 4*mbh**2/s**2*abs(wc['7']*fftv)**2 + 4*mbh/s*ff['v']*_Re(wc['7']*ffta*_Co(wc['v']))
+    bq = meson_quark[B]
+    label = bq+lep+lep
+    fftv = getfftv(s, par, B, ff, ff0, lep, wc)
+    ffta = getffta(s, par, B, ff, ff0, lep, wc)
+    return (abs(wc['C9_'+label])**2 + abs(wc['C10_'+label])**2)*ff['v']**2 + 4*mbh**2/s**2*abs(wc['C7_'+bq]*fftv)**2 + 4*mbh/s*ff['v']*_Re(wc['C7_'+bq]*ffta*_Co(wc['C9_'+label]))
 
 def getF2(s, par, B, ff, ff0, lep, wc):
     scale = config['renormalization scale']['bllgamma']
     mb = running.get_mb(par, scale, nf_out=5)
     mbh = mb/par['m_'+B]
-    gphi = -par['Bs->phi BSZ a0_T1']
-    fphi = -1./3*par['f_phi']
-    #WILL MOVE TO par
-    a1 = -0.13
-    fftv = ff['tv']+ff0['tv']-2*fphi*gphi*par['m_'+B]**2*s/par['m_phi']/(par['m_'+B]**2*s-par['m_phi']**2+1j*par['m_phi']/par['tau_phi'])+16/3*a1/wc['7']*par['f_'+B]/mb
-    ffta = ff['ta']+ff0['ta']-2*fphi*gphi*par['m_'+B]**2*s/par['m_phi']/(par['m_'+B]**2*s-par['m_phi']**2+1j*par['m_phi']/par['tau_phi'])
-    return (abs(wc['v'])**2 + abs(wc['a'])**2)*ff['a']**2 + 4*mbh**2/s**2*abs(wc['7']*ffta)**2 + 4*mbh/s*ff['a']*_Re(wc['7']*ffta*_Co(wc['v']))
+    bq = meson_quark[B]
+    label = bq+lep+lep
+    fftv = getfftv(s, par, B, ff, ff0, lep, wc)
+    ffta = getffta(s, par, B, ff, ff0, lep, wc)
+    return (abs(wc['C9_'+label])**2 + abs(wc['C10_'+label])**2)*ff['a']**2 + 4*mbh**2/s**2*abs(wc['C7_'+bq]*ffta)**2 + 4*mbh/s*ff['a']*_Re(wc['C7_'+bq]*ffta*_Co(wc['C9_'+label]))
 
     
 def B10(s, par, B, ff, ff0, lep, wc):
     F1 = getF1(s, par, B, ff, ff0, lep, wc)
     F2 = getF2(s, par, B, ff, ff0, lep, wc)
     mlh = par['m_'+lep]/par['m_'+B]
-    return (s + 4*mlh**2)*(F1 + F2) - 8*mlh**2*abs(wc['a'])**2*(ff['v']**2+ff['a']**2)
+    label = meson_quark[B]+lep+lep
+    return (s + 4*mlh**2)*(F1 + F2) - 8*mlh**2*abs(wc['C10_'+label])**2*(ff['v']**2+ff['a']**2)
 
 # B11 vanishes for the BR estimation
 
@@ -82,14 +98,12 @@ def B120(s, par, B, ff, ff0, lep, wc):
     scale = config['renormalization scale']['bllgamma']
     mb = running.get_mb(par, scale, nf_out=5)
     mbh = mb/par['m_'+B]
-    gphi = -par['Bs->phi BSZ a0_T1']
-    fphi = -1./3*par['f_phi']
-    #WILL MOVE TO par
-    a1 = -0.13
-    fftv = ff['tv']+ff0['tv']-2*fphi*gphi*par['m_'+B]**2*s/par['m_phi']/(par['m_'+B]**2*s-par['m_phi']**2+1j*par['m_phi']/par['tau_phi'])+16/3*a1/wc['7']*par['f_'+B]/mb
-    ffta = ff['ta']+ff0['ta']-2*fphi*gphi*par['m_'+B]**2*s/par['m_phi']/(par['m_'+B]**2*s-par['m_phi']**2+1j*par['m_phi']/par['tau_phi'])
     mlh = par['m_'+lep]/par['m_'+B]
-    return -16*mlh**2*(1-s)*(ff['v']*_Re(wc['v']*_Co(wc['a'])) + 2*mbh/s*_Re(_Co(wc['a'])*fftv*wc['7']))
+    bq = meson_quark[B]
+    label = bq+lep+lep
+    fftv = getfftv(s, par, B, ff, ff0, lep, wc)
+    ffta = getffta(s, par, B, ff, ff0, lep, wc)
+    return -16*mlh**2*(1-s)*(ff['v']*_Re(wc['C9_'+label]*_Co(wc['C10_'+label])) + 2*mbh/s*_Re(_Co(wc['C10_'+label])*fftv*wc['C7_'+bq]))
 
 
 def dG1dsMN(s, par, B, ff, ff0, lep, wc):
@@ -99,7 +113,9 @@ def dG1dsMN(s, par, B, ff, ff0, lep, wc):
     
 def dG2dsMN(s, par, B, ff, ff0, lep, wc):
     mlh = par['m_'+lep]/par['m_'+B]
-    pref = 16*prefactor(s, par, B, ff, lep, wc)*(par['f_'+B]/par['m_'+B]*abs(wc['a'])*mlh)**2/(1-s)
+    bq = meson_quark[B]
+    label = bq+lep+lep
+    pref = 16*prefactor(s, par, B, ff, lep, wc)*(par['f_'+B]/par['m_'+B]*abs(wc['C10_'+label])*mlh)**2/(1-s)
     return pref*(-8*sqrt(s-4*mlh**2)*sqrt(s)+4*atanh(sqrt(1-4*mlh**2/s))*(1+s-mlh**2*(1-s)**2))
 
 def dG12dsMN(s, par, B, ff, ff0, lep, wc):
@@ -120,12 +136,10 @@ def bllg_dbrdq2(q2, wc_obj, par, B, lep):
     ff0 = get_ff(0, par, B)
     label = meson_quark[B]+lep+lep
     wc = wctot_dict(wc_obj, label, scale, par)
-    #TO BE CHANGED
-    wc_eff = get_wceff(q2, wc, par, 'B0', 'K0', lep, scale)
     if q2 <= 4*ml**2 or q2 > mB**2:
         return 0
     else:
-        dBR = tauB * dGdsMN(q2/mB**2, par, B, ff, ff0, lep, wc_eff)
+        dBR = tauB * dGdsMN(q2/mB**2, par, B, ff, ff0, lep, wc)
         return dBR
 
 def bllg_dbrdq2_func(B, lep):
@@ -144,26 +158,30 @@ def bllg_dbrdq2_int_func(B, lep):
     return fct
 
 _tex = {'e': 'e', 'mu': '\mu', 'tau': r'\tau'}
-for l in ['e', 'mu', 'tau']:
+_tex_B = {'B0': r'\bar B^0', 'Bs': r'\bar B_s'}
 
-        _process_tex =  r"B_s\to" +_tex[l]+r"^+"+_tex[l]+r"^-\gamma"
-        _process_taxonomy = r'Process :: $b$ hadron decays :: FCNC decays :: $B\to \ell^+\ell^-\gamma$ :: $'
+_process_taxonomy = r'Process :: $b$ hadron decays :: FCNC decays :: $B\to \ell^+\ell^-\gamma$ :: $'
+
+for l in ['e', 'mu']:
+    for B in ['Bs', 'B0']:
+
+        _process_tex =  _tex_B[B]+r"\to" +_tex[l]+r"^+"+_tex[l]+r"^-\gamma"
 
 
         # binned branching ratio
-        _obs_name = "<dBR/dq2>(Bs->"+l+l+"gamma)"
+        _obs_name = "<dBR/dq2>("+B+"->"+l+l+"gamma)"
         _obs = Observable(name=_obs_name, arguments=['q2min', 'q2max'])
         _obs.set_description(r"Binned differential branching ratio of $" + _process_tex + r"$")
         _obs.tex = r"$\langle \frac{d\text{BR}}{dq^2} \rangle(" + _process_tex + r")$"
-        _obs.add_taxonomy(_process_taxonomy)
-        Prediction(_obs_name, bllg_dbrdq2_int_func('Bs', l))
+        _obs.add_taxonomy(_process_taxonomy + _process_tex + r"$")
+        Prediction(_obs_name, bllg_dbrdq2_int_func(B, l))
 
         # differential branching ratio
-        _obs_name = "dBR/dq2(Bs->"+l+l+"gamma)"
+        _obs_name = "dBR/dq2("+B+"->"+l+l+"gamma)"
         _obs = Observable(name=_obs_name, arguments=['q2'])
         _obs.set_description(r"Differential branching ratio of $" + _process_tex + r"$")
         _obs.tex = r"$\frac{d\text{BR}}{dq^2}(" + _process_tex + r")$"
-        _obs.add_taxonomy(_process_taxonomy)
-        Prediction(_obs_name, bllg_dbrdq2_func('Bs', l))
+        _obs.add_taxonomy(_process_taxonomy + _process_tex + r"$")
+        Prediction(_obs_name, bllg_dbrdq2_func(B, l))
 
 
