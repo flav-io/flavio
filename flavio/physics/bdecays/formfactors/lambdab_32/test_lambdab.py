2c71205d4ed99f00a3b6478dfc2fd196444c23cc
Felicia Volle <Felicia.Volle@gmail.com>
2021-02-03 14:32:35 +0100

    Checking QM and Lattice form factors at high q2

--- flavio/physics/bdecays/formfactors/lambdab_32/test_lambdab.py
+++ flavio/physics/bdecays/formfactors/lambdab_32/test_lambdab.py
@@ -30,11 +30,30 @@ class TestLambdabLambda1520_FF(unittest.TestCase):
         # Comparison to figure 9 in arXiv:2009.09313
         pred_sm(self, 'AFBl(Lambdab->Lambda(1520)mumu)', 16.0, -0.13, 0.02)
         pred_sm(self, 'AFBl(Lambdab->Lambda(1520)mumu)', 16.4, 0.0, 0.02)
-        pred_sm(self, 'AFBl(Lambdab->Lambda(1520)mumu)', 16.75, 0.3, 0.02)
+        pred_sm(self, 'AFBl(Lambdab->Lambda(1520)mumu)', 16.8, 0.32, 0.02)
 
         # Comparison to figure 7 in arXiv:2009.09313
         pred_sm(self, 'S_1cc(Lambdab->Lambda(1520)mumu)', 16.0, 0.6, 0.02)
         pred_sm(self, 'S_1cc(Lambdab->Lambda(1520)mumu)', 16.4, 0.6, 0.02)
-        pred_sm(self, 'S_1cc(Lambdab->Lambda(1520)mumu)', 16.8, 0.38, 0.06)
+        pred_sm(self, 'S_1cc(Lambdab->Lambda(1520)mumu)', 16.8, 0.44, 0.02)
+
+        
+        flavio.config['implementation']['Lambdab->Lambda(1520) form factor'] = 'Lambdab->Lambda(1520) MCN'
+
+        # Comparison to figure on slide 12 S.Meinel b-baryon FEST 2020
+        pred_sm(self, 'dBR/dq2(Lambdab->Lambda(1520)mumu)', 16.0, pow(5.4, -9), pow(0.2, -9), BRinv)
+        pred_sm(self, 'dBR/dq2(Lambdab->Lambda(1520)mumu)', 16.4, pow(2.2, -9), pow(0.2, -9), BRinv)
+        pred_sm(self, 'dBR/dq2(Lambdab->Lambda(1520)mumu)', 16.6, 0.0, pow(0.01, -9), BRinv)
+
+        # Comparison to figure on slide 14 S.Meinel b-baryon FEST 2020
+        pred_sm(self, 'AFBl(Lambdab->Lambda(1520)mumu)', 16.0, -0.16, 0.02)
+        pred_sm(self, 'AFBl(Lambdab->Lambda(1520)mumu)', 16.4, -0.08, 0.02)
+        pred_sm(self, 'AFBl(Lambdab->Lambda(1520)mumu)', 16.6, 0.0, 0.02)
+        pred_sm(self, 'AFBl(Lambdab->Lambda(1520)mumu)', 16.8, 0.22, 0.02)
+
+        # Comparison to figure on slide 13 S.Meinel b-baryon FEST 2020
+        pred_sm(self, 'S_1cc(Lambdab->Lambda(1520)mumu)', 16.0, 0.6, 0.02)
+        pred_sm(self, 'S_1cc(Lambdab->Lambda(1520)mumu)', 16.4, 0.6, 0.02)
+        pred_sm(self, 'S_1cc(Lambdab->Lambda(1520)mumu)', 16.8, 0.38, 0.02)
 
         
