e67ea14566b02e883082260d9380b7470498d32c
Matej Hudec <matejhudec@email.cz>
2021-02-05 12:13:43 +0100

    Correct calculation of LFV decays KL,KS->l1+l2-
    * The original `amplitudes` function renamed to `amplitudes_weak_eigst`
     as it actually yields amplitudes for K0, K0bar, not KL or KS.
    
    * A new `amplitudes` function, which newly takes also `K` (to be `'KL'` or `'KS'`)
     as an argument, has been implemented.
     It makes proper linear combinations of amplitudes for K0 and K0bar
     and changes by sqrt(2) the normalization of the amplitudes
     (cf. arXiv:1602.00881 with 1711.11030).
    
    * For l1=l2, `amplitudes` yields the imag/real parts of the S, P weak_eigs coefficients, as it should.
     Thus, taking real/imag parts is no longer tackled in `amplitudes_eff`,
     which now serves merely to add up short- and long-distance contributions, as it should.
    
    * Relative signs between SD and LD contributions now tackled in `amplitudes_eff`.
     It is in accordance with the original version of this file;
     looks differently than in arXiv:1711.11030, but it is due to different WC
     conventions (see my notes for details).

--- flavio/physics/kdecays/kll.py
+++ flavio/physics/kdecays/kll.py
@@ -9,8 +9,8 @@ from flavio.physics.common import add_dict
 from math import pi, sqrt
 
 
-def amplitudes(par, wc, l1, l2):
-    r"""Amplitudes P and S entering the $K\to\ell_1^+\ell_2^-$ observables.
+def amplitudes_weak_eigst(par, wc, l1, l2):
+    r"""Amplitudes P and S for the decay $\bar K^0\to\ell_1^+\ell_2^-$.
 
     Parameters
     ----------
@@ -37,6 +37,41 @@ def amplitudes(par, wc, l1, l2):
     return xi_t * P, xi_t * S
 
 
+def amplitudes(par, wc, K, l1, l2):
+    r"""Amplitudes P and S entering the $K_{L,S}\to\ell_1^+\ell_2^-$ observables.
+    
+    Parameters
+    ----------
+    
+    - `par`: parameter dictionary
+    - `wc`: Wilson coefficient dictionary
+    - `K`: should be `'KL'` or `'KS'`
+    - `l1` and `l2`: should be `'e'` or `'mu'`
+    """
+    # KL, KS are linear combinations of K0, K0bar. So are the amplitudes.
+    # Normalization differs by sqrt(2) from `amplitudes_weak_eigst`.
+    P_K0bar, S_K0bar = amplitudes_weak_eigst(par, wc, l1, l2)
+    if l1 != l2:
+        P_aux, S_aux = amplitudes_weak_eigst(par, wc, l2, l1)
+        S_K0 = -S_aux.conjugate()
+        P_K0 = P_aux.conjugate()
+        if K == 'KL':
+            sig = +1
+        elif K == 'KS':
+            sig = -1
+        S = (S_K0 + sig * S_K0bar) / 2
+        P = (P_K0 + sig * P_K0bar) / 2
+    # Simplified expressions for special cases. See also arXiv:1711.11030.
+    elif l1 == l2:
+        if K == 'KL':
+            S = -1j * S_K0bar.imag 
+            P = P_K0bar.real
+        elif K == 'KS':
+            S = -S_K0bar.real
+            P = -1j * P_K0bar.imag
+    return P, S
+
+
 def amplitudes_LD(par, K, l):
     r"""Long-distance amplitudes entering the $K\to\ell^+\ell^-$ observables."""
     ml = par['m_' + l]
@@ -46,43 +81,43 @@ def amplitudes_LD(par, K, l):
     # numbers extracted from arXiv:1711.11030
     if K == 'KS':
         ASgaga = 2.49e-4 * (-2.821 + 1.216j)
-        SLD = +pre * ASgaga
+        SLD = pre * ASgaga
         PLD = 0
     elif K == 'KL':
         ALgaga = 2.02e-4 * (par['chi_disp(KL->gammagamma)'] - 5.21j)
         SLD = 0
-        PLD = -pre * ALgaga
+        PLD = pre * ALgaga
     return SLD, PLD
 
 
 def amplitudes_eff(par, wc, K, l1, l2, ld=True):
     r"""Effective amplitudes entering the $K\to\ell_1^+\ell_2^-$ observables."""
-    P, S = amplitudes(par, wc, l1, l2)
+    P, S = amplitudes(par, wc, K, l1, l2)
     if l1 != l2 or not ld:
         SLD = 0
         PLD = 0
     else:
         SLD, PLD = amplitudes_LD(par, K, l1)
-    if K == 'KS':
-        Peff = P.imag + PLD
-        Seff = S.real + SLD
-    if K == 'KL':
-        Peff = P.real + PLD
-        Seff = S.imag + SLD
+    # The relative sign due to different conventions. Cf. my notes.
+    Peff = P - PLD
+    Seff = S - SLD
     return Peff, Seff
 
 
 def get_wc(wc_obj, par, l1, l2):
     scale = config['renormalization scale']['kdecays']
-    label = 'sd' + l1 + l2
-    wcnp = wc_obj.get_wc(label, scale, par)
-    if l1 == l2:
+    if l1 == l2:   # (l1,l2) == ('e','e') or ('mu','mu')
+        label = 'sd' + l1 + l2
+        wcnp = wc_obj.get_wc(label, scale, par)
         # include SM contributions for LF conserving decay
         _c = wilsoncoefficients_sm_sl(par, scale)
         xi_t = ckm.xi('t', 'sd')(par)
         xi_c = ckm.xi('c', 'sd')(par)
         wcsm = {'C10_sd' + l1 + l2: _c['C10_t'] + xi_c / xi_t * _c['C10_c']}
-    else:
+    elif {l1,l2} == {'e','mu'}:
+        # Both flavor combinations relevant due to K0-K0bar mixing
+        wcnp = {**wc_obj.get_wc('sdemu', scale, par),
+                **wc_obj.get_wc('sdmue', scale, par)}
         wcsm = {}
 
     return add_dict((wcsm, wcnp))
