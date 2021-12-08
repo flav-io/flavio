e44bdba5a665834c9e7b36ee21e7ebf6fc4e5edd
Michael Schmidt <m.schmidt@unsw.edu.au>
2021-12-08 22:41:19 +1100

    trying to implement a unit test

--- flavio/physics/quarkonium/test_Vll.py
+++ flavio/physics/quarkonium/test_Vll.py
@@ -7,5 +7,6 @@ par = flavio.default_parameters.get_central_all()
 ### implement test
 class TestVll(unittest.TestCase):
     def test_sm(self):
-        self.assertEqual(flavio.sm_prediction('BR(J/psi->ee)'), 1.) #par['tau_Bc_SM'])
-#        self.assertAlmostEqual(flavio.sm_prediction('tau_Bc'), par['tau_Bc'], delta=0.1 * par['tau_Bc'])
+        br=par['BR(J/psi->e+e-)']
+        self.assertEqual(flavio.sm_prediction('BR(J/psi->ee)'),br ) #par['tau_Bc_SM'])
+        self.assertAlmostEqual(flavio.sm_prediction('BR(J/psi->ee)'), br,delta=0.1*br)
