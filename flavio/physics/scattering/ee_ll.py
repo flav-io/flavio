2ab9c2ec7f49ad7f4cbdfe575ca96ac06b5cf7d9
BAllanach <B.C.Allanach@damtp.cam.ac.uk>
2023-02-21 14:45:48 +0100

    Generalised e+e- -> l+l- to take a family integer argument

---
+++ flavio/physics/scattering/ee_ll.py
@@ -0,0 +1,63 @@
+r"""Functions for $e^+ e^-\to l^+ l^- of various flavours"""
+# Written by Ben Allanach 
+
+import flavio
+import numpy as np
+
+# predicted difference to SM correction from SMEFT operators of e+e-->mumu for LEP2 energy E and family fam
+def ee_ll(C, par, E, fam):
+    # Check energy E is correct
+    if (E != 182.7 and E != 188.6 and E != 191.6 and E != 195.5 and E != 199.5 and E!= 201.6 and E!= 206.6):
+        raise ValueError('ee_ll called with incorrect LEP2 energy {} GeV.'.format(E))
+        
+    # For now, delta g couplings have been NEGLECTED
+    PI = 3.141592653589793
+    s = E * E
+    mz = par['m_Z']
+    GF = par['GF']
+    alpha = par['alpha_e']
+    s2w   = par['s2w']
+    gzeL  = -0.5 + s2w
+    gzeR  = s2w
+    eSq   = 4 * PI * alpha
+    gLsq  = eSq / s2w
+    gYsq  = gLsq * s2w / (1. - s2w)
+    vSq   = 1. / (np.sqrt(2.) * GF)
+    res   = 0
+    if (fam == 2 or fam == 3):
+        res   = 1. / (24. * PI * vSq) * (
+            eSq * (C['ll_11' + str(fam) + str(fam)] +
+                   C['ll_1' + str(fam) + str(fam) + '1'] +
+                   C['ee_11' + str(fam) + str(fam)] +
+                   C['le_11' + str(fam) + str(fam)] +
+                   C['le_' + str(fam) + str(fam) + '11']) +
+            s * (gLsq + gYsq) / (s - mz**2) * (
+                gzeL**2 * (C['ll_11' + str(fam) + str(fam)] +
+                           C['ll_1' + str(fam) + str(fam) + '1']) +
+                gzeR**2 *  C['ee_11' + str(fam) + str(fam)] +
+                gzeL * gzeR * (C['le_11' + str(fam) + str(fam)] +
+                               C['le_' + str(fam) + str(fam)])
+            )
+        )
+    else:
+        raise ValueError('ee_ll called with incorrect family {}'.format(fam))
+    # The following numerical check made it looks like the constants have the correct values, meaning the conventions are understtod: 21/2/23
+    # print('# DEBUG: MZ=', np.sqrt(vSq * (gLsq + gYsq) / 4.),' MW=', np.sqrt(gLsq * vSq / 4.),' PI=',PI, ' v=',np.sqrt(vSq))
+    return res 
+
+def ee_ll_obs(wc_obj, par, E, fam):
+    scale = flavio.config['renormalization scale']['ee_ww'] # Use LEP2 renorm scale
+    C = wc_obj.get_wcxf(sector='all', scale=scale, par=par,
+                        eft='SMEFT', basis='Warsaw')
+    return ee_ll(C, par, E, fam)
+
+_process_tex = r"e^+e^- \to l^+l^-"
+_process_taxonomy = r'Process :: $e^+e^-$ scattering :: $e^+e^-\to l^+l^-$ :: $' + _process_tex + r"$"
+
+_obs_name = "dsigma(ee->ll)"
+_obs = flavio.classes.Observable(_obs_name)
+_obs.arguments = ['E', 'fam']
+flavio.classes.Prediction(_obs_name, ee_ll_obs)
+_obs.set_description(r"Cross section of $" + _process_tex + r"$ at energy $E$ minus that of the SM")
+_obs.tex = r"$d\sigma(" + _process_tex + r")$"
+_obs.add_taxonomy(_process_taxonomy)
