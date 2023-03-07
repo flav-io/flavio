26153c11161c85283855599ebf73c3b15c6a6838
Peter Stangl <peter.stangl@ph.tum.de>
2023-03-07 15:21:07 +0100

    use quad by default for numerical integrations

--- flavio/math/integrate.py
+++ flavio/math/integrate.py
@@ -6,11 +6,17 @@ try:
 except ImportError:
     from scipy.integrate.quadrature import AccuracyWarning # scipy <= 1.4.1
 
-def nintegrate(f, a, b, epsrel=0.005, **kwargs):
+def nintegrate_quadrature(f, a, b, epsrel=0.005, **kwargs):
     with warnings.catch_warnings():
         # ignore AccuracyWarning that is issued when an integral is zero
         warnings.filterwarnings("ignore", category=AccuracyWarning)
         return scipy.integrate.quadrature(f, a, b, rtol=epsrel, tol=0, vec_func=False, **kwargs)[0]
+
+def nintegrate(f, a, b, epsrel=0.005, **kwargs):
+    with warnings.catch_warnings():
+        # ignore AccuracyWarning that is issued when an integral is zero
+        warnings.filterwarnings("ignore", category=AccuracyWarning)
+        return scipy.integrate.quad(f, a, b, epsabs=0, epsrel=epsrel, **kwargs)[0]
 
 def nintegrate_fast(f, a, b, N=5, **kwargs):
     x = np.linspace(a,b,N)
