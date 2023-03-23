3a1a17029cb49c93dc611be6be4adcab40c3d7fc
Aleks Smolkovic <smolkovic@itp.unibe.ch>
2023-03-23 17:06:08 +0100

    implementation of b>dll observables

--- flavio/physics/bdecays/common.py
+++ flavio/physics/bdecays/common.py
@@ -26,6 +26,7 @@ meson_quark = {
 ('B0','rho0'): 'bd',
 ('B+','rho+'): 'bd',
 ('B0','omega'): 'bd',
+('Bs','K*0'): 'bd',
 ('Bs','K+'): 'bu',
 ('Bs','K*+'): 'bu',
 ('B0','pi+'): 'bu',
@@ -55,6 +56,7 @@ meson_spectator = {
 ('B+','K*+'): 'u',
 ('B0','K*0'): 'd',
 ('Bs','phi'): 's',
+('Bs', 'K*0'): 's',
 }
 
 quark_charge = {
