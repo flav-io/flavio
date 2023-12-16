58df88d57596007c6243ef2cdbc2f5d27f368fee
Peter Stangl <peter.stangl@ph.tum.de>
2023-12-16 03:09:25 +0100

    update B->K form factors to 2305.06301

--- flavio/classes.py
+++ flavio/classes.py
@@ -505,8 +505,8 @@ class ParameterConstraints(Constraints):
         flavio.physics.bdecays.formfactors.b_v.bsz_parameters.bsz_load('v2', 'LCSR-Lattice', ('B->K*', 'Bs->phi', 'Bs->K*'), self)
 
         # Read default parameters for B->P form factors
-        flavio.physics.bdecays.formfactors.b_p.bsz_parameters.gkvd_load('v1', 'LCSR-Lattice', ('B->K', 'B->pi'), self)
-        flavio.physics.bdecays.formfactors.b_p.bcl_parameters_lmvd.load_parameters('data/arXiv-2102.07233v2/LCSR-LQCD_mod_BCL_params_K=4.yaml', self)
+        flavio.physics.bdecays.formfactors.b_p.bsz_parameters.load_ffs_eos('data/arXiv-2305-06301v1/BSZ-parameters-N2.yaml', 'B->K::FormFactors[parametric,BSZ]@GRvDV:2023A', r'::alpha\^(.*)_(.*)@BSZ2015', r' BSZ a\2_\1', self) # B->K
+        flavio.physics.bdecays.formfactors.b_p.bcl_parameters_lmvd.load_parameters('data/arXiv-2102.07233v2/LCSR-LQCD_mod_BCL_params_K=4.yaml', self) # B->pi
 
         # Read default parameters for Lambdab->Lambda form factors
         flavio.physics.bdecays.formfactors.lambdab_12.lattice_parameters.lattice_load_ho(self)
